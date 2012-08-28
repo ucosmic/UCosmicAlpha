1486836cb6b1632f07400315ea062739cbdc0977
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2012-08-28 18:26:19 -0400

    Bootstrap UI.

--- UCosmicLayout3/App_Start/BundleConfig.cs
+++ UCosmicLayout3/App_Start/BundleConfig.cs
@@ -8,36 +8,44 @@ namespace UCosmicLayout3
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            // jQuery
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/scripts/jquery-{version}.js"));
 
+            // jQuery UI
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                        "~/scripts/jquery-ui-{version}.js"));
 
+            // jquery validation, plus ms unobtrusive validation and ajax
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+                        "~/scripts/jquery.unobtrusive*",
+                        "~/scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            // bootstrap css bundle
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                        "~/styles/reset.css",
+                        "~/styles/layout.css",
+                        "~/styles/forms.css"));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            // jQuery UI theme
+            bundles.Add(new StyleBundle("~/content/themes/base/css").Include(
+                        "~/content/themes/base/jquery.ui.core.css",
+                        "~/content/themes/base/jquery.ui.resizable.css",
+                        "~/content/themes/base/jquery.ui.selectable.css",
+                        "~/content/themes/base/jquery.ui.accordion.css",
+                        "~/content/themes/base/jquery.ui.autocomplete.css",
+                        "~/content/themes/base/jquery.ui.button.css",
+                        "~/content/themes/base/jquery.ui.dialog.css",
+                        "~/content/themes/base/jquery.ui.slider.css",
+                        "~/content/themes/base/jquery.ui.tabs.css",
+                        "~/content/themes/base/jquery.ui.datepicker.css",
+                        "~/content/themes/base/jquery.ui.progressbar.css",
+                        "~/content/themes/base/jquery.ui.theme.css"));
         }
     }
 }
