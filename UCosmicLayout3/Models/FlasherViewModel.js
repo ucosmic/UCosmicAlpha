ce30aebd471208efa6f98505de99cb79ad4c6574
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2012-08-29 19:13:57 -0400

    Tenantize flasher.

--- UCosmicLayout3/Models/FlasherViewModel.js
+++ UCosmicLayout3/Models/FlasherViewModel.js
@@ -16,14 +16,45 @@
             });
         };
 
+        self.flash = function(text) {
+            self.text(undefined);
+            if (text) self.text(text);
+        };
+
+        self.ticks = 0;
+        self.tickInterval = undefined;
+        self.tickCount = ko.observable(10);
+
+        self.tick = function () {
+            if (self.ticks <= 0) {
+                self.ticks = 0;
+                self.tickCount(self.ticks);
+                clearInterval(self.tickInterval);
+                self.dismiss();
+            }
+            else {
+                --self.ticks;
+                self.tickCount(self.ticks);
+            }
+        };
+
         ko.computed(function () {
             if (self.text()) {
+                clearInterval(self.tickInterval);
+                self.ticks = 9;
+                self.tickCount(self.ticks);
+                self.tickInterval = setInterval(function () {
+                    self.tick();
+                }, 1000);
                 self.$element().hide();
                 self.$element().removeClass('hide');
                 self.$element().fadeIn('fast');
-                setTimeout(function () {
-                    self.dismiss();
-                }, 10000);
+            }
+            else {
+                if (self.tickInterval)
+                    clearInterval(self.tickInterval);
+                if (self.element)
+                    self.$element().addClass('hide');
             }
         });
     }
