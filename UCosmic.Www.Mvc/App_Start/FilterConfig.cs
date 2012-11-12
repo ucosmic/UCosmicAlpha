03f2b74e4476af402a5325c8f3811fb99991a456
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2012-11-12 11:22:35 -0500

    Globally enforce HTTPS in release mode.

--- UCosmic.Www.Mvc/App_Start/FilterConfig.cs
+++ UCosmic.Www.Mvc/App_Start/FilterConfig.cs
@@ -7,6 +7,10 @@ namespace UCosmic.Www.Mvc
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+
+#if !DEBUG
+            filters.Add(new EnforceHttpsAttribute());
+#endif
         }
     }
 }
---
+++ UCosmic.Www.Mvc/Helpers/EnforceHttpsAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace UCosmic.Www.Mvc
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+    public class EnforceHttpsAttribute : RequireHttpsAttribute
+    {
+        // ReSharper disable RedundantOverridenMember
+        protected override void HandleNonHttpsRequest(AuthorizationContext filterContext)
+        {
+            base.HandleNonHttpsRequest(filterContext);
+
+#if APPHARBOR
+            // RequireHttps does not work in AppHarbor: http://stackoverflow.com/a/8977247/304832
+            var xForwardedProtoHeader = filterContext.HttpContext.Request.Headers["X-Forwarded-Proto"];
+            if (filterContext.Result is RedirectResult &&
+                "https".Equals(xForwardedProtoHeader, StringComparison.InvariantCultureIgnoreCase))
+                filterContext.Result = null;
+#endif
+        }
+        // ReSharper restore RedundantOverridenMember
+    }
+}
