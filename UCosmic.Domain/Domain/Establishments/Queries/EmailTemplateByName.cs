d92168d6e2ee3a706b5253e3610dd593143303a4
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2012-12-19 15:10:48 -0500

    Add email template entity seeder and its command / query dependences.

---
+++ UCosmic.Domain/Domain/Establishments/Queries/EmailTemplateByName.cs
@@ -0,0 +1,34 @@
+﻿namespace UCosmic.Domain.Establishments
+{
+    public class EmailTemplateByName : IDefineQuery<EmailTemplate>
+    {
+        public string Name { get; set; }
+        public int? EstablishmentId { get; set; }
+    }
+
+    public class HandleEmailTemplateByNameQuery : IHandleQueries<EmailTemplateByName, EmailTemplate>
+    {
+        private readonly IQueryEntities _entities;
+
+        public HandleEmailTemplateByNameQuery(IQueryEntities entities)
+        {
+            _entities = entities;
+        }
+
+        public EmailTemplate Handle(EmailTemplateByName query)
+        {
+            // get the template
+            var template = _entities.Query<EmailTemplate>()
+                .ByName(query.Name, query.EstablishmentId)
+            ;
+
+            // fall back to default
+            if (template == null && query.EstablishmentId.HasValue)
+                template = _entities.Query<EmailTemplate>()
+                    .ByName(query.Name, null)
+                ;
+
+            return template;
+        }
+    }
+}
