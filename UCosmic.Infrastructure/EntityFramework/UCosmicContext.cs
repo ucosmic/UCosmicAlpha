097cf00828dbbe5e7696894fd184294c40a68be5
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2013-02-04 12:32:34 -0500

    Handle DbContext SaveChanges to log of uncaught EntityValidationExceptions.

--- UCosmic.Infrastructure/EntityFramework/UCosmicContext.cs
+++ UCosmic.Infrastructure/EntityFramework/UCosmicContext.cs
@@ -3,10 +3,16 @@ using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration;
 using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Net.Mail;
 using System.Reflection;
+using System.Text;
+using UCosmic.Configuration;
 using UCosmic.Domain;
+using UCosmic.Logging;
+using UCosmic.Mail;
 
 namespace UCosmic.EntityFramework
 {
@@ -84,6 +90,46 @@ namespace UCosmic.EntityFramework
             {
                 dynamic configurationInstance = Activator.CreateInstance(typeToRegister);
                 modelBuilder.Configurations.Add(configurationInstance);
+            }
+        }
+
+        public override int SaveChanges()
+        {
+            try
+            {
+                return base.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                var config = new DotNetConfigurationManager();
+                if (!config.EmergencyMailAddresses.Any()) throw;
+                var mailSender = new SmtpMailSender(config, new ElmahExceptionLogger(
+                    config.DefaultMailFromAddress, config.EmergencyMailAddresses));
+                var message = new MailMessage(
+                    config.DefaultMailFromAddress, config.EmergencyMailAddresses.First())
+                {
+                    Subject = "UCosmic caught DbEntityValidationException during SaveChanges.",
+                };
+                if (config.EmergencyMailAddresses.Count() > 1)
+                    foreach (var emergencyAddress in config.EmergencyMailAddresses.Skip(1))
+                        message.CC.Add(emergencyAddress);
+                var body = new StringBuilder();
+                foreach (var entry in ex.EntityValidationErrors)
+                {
+                    var entityType = entry.Entry.Entity.GetType();
+                    foreach (var error in entry.ValidationErrors)
+                    {
+                        body.Append(entityType.Name);
+                        body.Append('.');
+                        body.Append(error.PropertyName);
+                        body.Append(": \r\n");
+                        body.Append(error.ErrorMessage);
+                        body.Append("\r\n\r\n");
+                    }
+                }
+                message.Body = body.ToString();
+                mailSender.Send(message);
+                throw;
             }
         }
     }
