651eeeba1d5fb02ebcca3da9548287bae0ea54d3
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2013-03-26 14:20:22 -0400

    Very basic file storage implementations for local filesystem & azure blobs.

---
+++ UCosmic.Infrastructure/BinaryData/AzureBlobStorage.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace UCosmic.BinaryData
+{
+    public class AzureBlobStorage : IStoreBinaryData
+    {
+        private readonly ConnectionStringSettings _connectionString;
+        private readonly CloudStorageAccount _storageAccount;
+        private readonly CloudBlobClient _blobClient;
+
+        public AzureBlobStorage(string connectionStringName)
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings[connectionStringName];
+            _storageAccount = CloudStorageAccount.Parse(_connectionString.ConnectionString);
+            _blobClient = _storageAccount.CreateCloudBlobClient();
+        }
+
+        private string GetContainerName(string path)
+        {
+            var pathParts = path.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
+            return pathParts.First();
+        }
+
+        private string GetBlobName(string path)
+        {
+            var pathParts = path.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            pathParts.Remove(pathParts.First());
+            var fileName = string.Join("/", pathParts);
+            return fileName;
+        }
+
+        public bool Exists(string path)
+        {
+            var containerName = GetContainerName(path);
+            var blobName = GetBlobName(path);
+            var containerReference = _blobClient.GetContainerReference(containerName);
+            var blockBlobReference = containerReference.GetBlockBlobReference(blobName);
+
+            return containerReference.Exists() && blockBlobReference.Exists();
+        }
+
+        public void Put(string path, byte[] data, bool overwrite = false)
+        {
+            // disallow file replacement unless specified in method invocation
+            if (!overwrite && Exists(path))
+                throw new InvalidOperationException(string.Format(
+                    "A file already exists at the path '{0}'. To overwrite this file, invoke this method with overwrite == true.", path));
+
+            var containerName = GetContainerName(path);
+            var blobName = GetBlobName(path);
+
+            var containerReference = _blobClient.GetContainerReference(containerName);
+
+            containerReference.CreateIfNotExists();
+            var blockBlobReference = containerReference.GetBlockBlobReference(blobName);
+
+            using (var stream = new MemoryStream(data, 0, data.Length))
+            {
+                blockBlobReference.UploadFromStream(stream);
+            }
+        }
+
+        public byte[] Get(string path)
+        {
+            var containerName = GetContainerName(path);
+            var blobName = GetBlobName(path);
+            var containerReference = _blobClient.GetContainerReference(containerName);
+            var blockBlobReference = containerReference.GetBlockBlobReference(blobName);
+
+            using (var stream = new MemoryStream())
+            {
+                blockBlobReference.DownloadToStream(stream);
+                return stream.ToArray();
+            }
+        }
+
+        public void Delete(string path)
+        {
+            var containerName = GetContainerName(path);
+            var blobName = GetBlobName(path);
+            var containerReference = _blobClient.GetContainerReference(containerName);
+            var blockBlobReference = containerReference.GetBlockBlobReference(blobName);
+
+            blockBlobReference.Delete();
+        }
+    }
+}
---
+++ UCosmic.Infrastructure/BinaryData/IisFileStorage.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Web.Hosting;
+
+namespace UCosmic.BinaryData
+{
+    // stores binary data as files on the local filesystem
+    public class IisFileStorage : IStoreBinaryData
+    {
+        private static string Root
+        {
+            // store all binary data in App_Data subdirectory
+            get { return HostingEnvironment.MapPath("~/App_Data/binary-data"); }
+        }
+
+        private static string GetFullPath(string relativePath)
+        {
+            // combine root with relative path for System.IO usage
+            return Path.Combine(Root, relativePath);
+        }
+
+        public bool Exists(string path)
+        {
+            // this does not work for directories, only files
+            var fullPath = GetFullPath(path);
+            var exists = File.Exists(fullPath);
+            return exists;
+        }
+
+        public void Put(string path, byte[] data, bool overwrite = false)
+        {
+            // disallow file replacement unless specified in method invocation
+            if (!overwrite && Exists(path))
+                throw new InvalidOperationException(string.Format(
+                    "A file already exists at the path '{0}'. To overwrite this file, invoke this method with overwrite == true.", path));
+
+            // create directories if they do not already exist
+            var fullPath = GetFullPath(path);
+            var directoryToCheck = "";
+            var directories = fullPath.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            directories.Remove(directories.Last()); // last element will be the file name, not a directory
+            foreach (var directory in directories)
+            {
+                directoryToCheck = directoryToCheck != ""
+                    ? string.Format("{0}/{1}", directoryToCheck, directory)
+                    : directory;
+                if (!Directory.Exists(directoryToCheck))
+                    Directory.CreateDirectory(directoryToCheck);
+            }
+
+            // create the file
+            using (var fileStream = File.Create(fullPath))
+            {
+                try
+                {
+                    fileStream.Write(data, 0, data.Length);
+                }
+                finally
+                {
+                    fileStream.Close();
+                }
+            }
+        }
+
+        public byte[] Get(string path)
+        {
+            var fullPath = GetFullPath(path);
+
+            // return null when file does not exist
+            if (!Exists(path)) return null;
+
+            var data = File.ReadAllBytes(fullPath);
+            return data;
+        }
+
+        public void Delete(string path)
+        {
+            // do nothing unless file exists
+            var fullPath = GetFullPath(path);
+            if (!Exists(path)) return;
+
+            File.Delete(fullPath);
+        }
+    }
+}
