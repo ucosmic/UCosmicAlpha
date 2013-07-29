fac5daf5f301be2f369179aa488e0e811f7d8308
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2013-07-29 09:53:25 -0400

    Move agreement participant related actions to separate api controller.

--- UCosmic.Web.Mvc/ApiControllers/Agreements/AgreementsController.cs
+++ UCosmic.Web.Mvc/ApiControllers/Agreements/AgreementsController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -12,12 +11,10 @@ using AutoMapper;
 using FluentValidation;
 using UCosmic.Domain.Agreements;
 using UCosmic.Domain.Identity;
-using UCosmic.Domain.Places;
 using UCosmic.Web.Mvc.Models;
 
 namespace UCosmic.Web.Mvc.ApiControllers
 {
-    //[DefaultApiHttpRouteConvention]
     [RoutePrefix("api/agreements")]
     public class AgreementsController : ApiController
     {
@@ -97,86 +94,6 @@ namespace UCosmic.Web.Mvc.ApiControllers
                     Mapper.Map<ValidationResultApiModel>(validationResult));
 
             return Request.CreateResponse(HttpStatusCode.OK);
-        }
-
-        [GET("{domain}/partners")]
-        public IEnumerable<AgreementParticipantApiModel> GetPartners(string domain)
-        {
-            var query = new PartnerParticipantsByOwnerDomain(User, domain)
-            {
-                EagerLoad = new Expression<Func<AgreementParticipant, object>>[]
-                {
-                    x => x.Establishment.Location,
-                    x => x.Establishment.Names.Select(y => y.TranslationToLanguage),
-                }
-            };
-            var partners = _queryProcessor.Execute(query);
-
-            // convert to model
-            var models = Mapper.Map<AgreementParticipantApiModel[]>(partners);
-
-            return models;
-        }
-
-        [GET("{domain}/partners/places/{placeType?}")]
-        public IEnumerable<AgreementPlaceApiModel> GetPartnerPlaces(string domain, string placeType = null)
-        {
-            var query = new PartnerPlacesByOwnerDomain(User, domain)
-            {
-                EagerLoad = new Expression<Func<Place, object>>[]
-                {
-                    x => x.GeoPlanetPlace.Type,
-                    //x => x.Ancestors.Select(y => y.Ancestor.GeoPlanetPlace.Type),
-                },
-                OrderBy = new Dictionary<Expression<Func<Place, object>>, OrderByDirection>
-                {
-                    { x => x.OfficialName, OrderByDirection.Ascending },
-                },
-            };
-            if ("countries".Equals(placeType, StringComparison.OrdinalIgnoreCase))
-                query.GroupBy = PlaceGroup.Countries;
-            if ("continents".Equals(placeType, StringComparison.OrdinalIgnoreCase))
-                query.GroupBy = PlaceGroup.Continents;
-            var places = _queryProcessor.Execute(query);
-
-            // convert to model
-            var models = Mapper.Map<AgreementPlaceApiModel[]>(places);
-
-            return models;
-        }
-
-        [GET("{agreementId:int}/participants")]
-        public IEnumerable<AgreementParticipantApiModel> GetParticipants(int agreementId)
-        {
-            var entities = _queryProcessor.Execute(new ParticipantsByAgreementId(User, agreementId)
-            {
-                EagerLoad = new Expression<Func<AgreementParticipant, object>>[]
-                {
-                    x => x.Establishment.Names,
-                },
-                OrderBy = new Dictionary<Expression<Func<AgreementParticipant, object>>, OrderByDirection>
-                {
-                    { x => x.IsOwner, OrderByDirection.Descending },
-                },
-            });
-            if (entities == null || !entities.Any()) throw new HttpResponseException(HttpStatusCode.NotFound);
-            var models = Mapper.Map<AgreementParticipantApiModel[]>(entities);
-            return models;
-        }
-
-        [GET("{agreementId:int}/participant/{establishmentId}")]
-        public AgreementParticipantApiModel GetParticipant(int agreementId, int establishmentId)
-        {
-            var entity = _queryProcessor.Execute(new ParticipantByEstablishmentId(User, establishmentId, agreementId)
-            {
-                EagerLoad = new Expression<Func<AgreementParticipant, object>>[]
-                {
-                    x => x.Establishment.Names,
-                },
-            });
-            if (entity == null) throw new HttpResponseException(HttpStatusCode.NotFound);
-            var model = Mapper.Map<AgreementParticipantApiModel>(entity);
-            return model;
         }
     }
 }
