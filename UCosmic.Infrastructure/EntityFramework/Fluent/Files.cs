6bb877e1857c8cd4ab89644e066aa84456ebeff2
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2013-09-23 16:54:24 -0400

    Prepare schema update SQL for next deployment.

--- UCosmic.Infrastructure/EntityFramework/Fluent/Files.cs
+++ UCosmic.Infrastructure/EntityFramework/Fluent/Files.cs
@@ -3,40 +3,6 @@ using UCosmic.Domain.Files;
 
 namespace UCosmic.EntityFramework
 {
-    public class LooseFileOrm : RevisableEntityTypeConfiguration<LooseFile>
-    {
-        public LooseFileOrm()
-        {
-            ToTable(typeof(LooseFile).Name, DbSchemaName.Files);
-        }
-    }
-
-    public class LoadableFileOrm : EntityTypeConfiguration<LoadableFile>
-    {
-        public LoadableFileOrm()
-        {
-            ToTable(typeof(LoadableFile).Name, DbSchemaName.Files);
-
-            HasRequired(p => p.Binary)
-                .WithRequiredPrincipal(d => d.Owner)
-                .WillCascadeOnDelete(true)
-            ;
-        }
-    }
-
-    public class LoadableFileBinaryOrm : EntityTypeConfiguration<LoadableFileBinary>
-    {
-        public LoadableFileBinaryOrm()
-        {
-            ToTable(typeof(LoadableFileBinary).Name, DbSchemaName.Files);
-
-            HasRequired(d => d.Owner)
-                .WithRequiredDependent(p => p.Binary)
-                .WillCascadeOnDelete(true)
-            ;
-        }
-    }
-
     public class UploadOrm : EntityTypeConfiguration<Upload>
     {
         public UploadOrm()
@@ -52,17 +18,6 @@ namespace UCosmic.EntityFramework
         {
             ToTable(typeof(ExternalFile).Name, DbSchemaName.Files);
 
-            Property(p => p.MimeType).IsRequired().HasMaxLength(256);
-        }
-    }
-
-    public class ImageOrm : EntityTypeConfiguration<Image>
-    {
-        public ImageOrm()
-        {
-            ToTable(typeof(Image).Name, DbSchemaName.Files);
-
-            Property(p => p.Title).IsOptional().HasMaxLength(64);
             Property(p => p.MimeType).IsRequired().HasMaxLength(256);
         }
     }
