c65e7ea2e9f0c5b9868e357b87f561e596aceb38
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2013-10-10 15:01:11 -0400

    Add more sorting complexity to my activities list.

--- UCosmic.Web.Mvc/Models/Activities/MyActivitiesInputModel.cs
+++ UCosmic.Web.Mvc/Models/Activities/MyActivitiesInputModel.cs
@@ -35,9 +35,34 @@ namespace UCosmic.Web.Mvc.Models
                 // third sort by title A-Z
                 var defaultOrderBy = new Dictionary<Expression<Func<Activity, object>>, OrderByDirection>
                 {
+                    // this puts all ongoings at the top
                     { x => x.Values.FirstOrDefault(y => y.ModeText == y.Activity.ModeText).OnGoing, OrderByDirection.Descending },
+
+                    // sort by year descending. Year comes from end date first, or start date if end date is null, or int.MinValue if start date is null
+                    // this will group the list by year descending, essentially creating 1 sort group for each coalesced year
+                    { x => x.Values.FirstOrDefault(y => y.ModeText == y.Activity.ModeText).EndsOn.HasValue
+                        ? x.Values.FirstOrDefault(y => y.ModeText == y.Activity.ModeText).EndsOn.Value.Year
+                        : x.Values.FirstOrDefault(y => y.ModeText == y.Activity.ModeText).StartsOn.HasValue
+                            ? x.Values.FirstOrDefault(y => y.ModeText == y.Activity.ModeText).StartsOn.Value.Year
+                            : int.MinValue, OrderByDirection.Descending },
+
+                    // activities that have both a start date & end date in same year come at the top (true above false)
+                    { x => x.Values.FirstOrDefault(y => y.ModeText == y.Activity.ModeText).StartsOn.HasValue
+                        && x.Values.FirstOrDefault(y => y.ModeText == y.Activity.ModeText).EndsOn.HasValue
+                        && x.Values.FirstOrDefault(y => y.ModeText == y.Activity.ModeText).StartsOn.Value.Year
+                            == x.Values.FirstOrDefault(y => y.ModeText == y.Activity.ModeText).EndsOn.Value.Year
+                        , OrderByDirection.Descending },
+
+                    // activities that have both start & end come at bottom (true below false) because they will not be in same year
+                    // after the sort expression above puts ones in same year at the top
+                    { x => x.Values.FirstOrDefault(y => y.ModeText == y.Activity.ModeText).EndsOn.HasValue
+                        && x.Values.FirstOrDefault(y => y.ModeText == y.Activity.ModeText).StartsOn.HasValue, OrderByDirection.Ascending },
+
+                    // so far all date sorting has been by year: use this to also sort by month & day descending
                     { x => x.Values.FirstOrDefault(y => y.ModeText == y.Activity.ModeText).EndsOn
                         ?? x.Values.FirstOrDefault(y => y.ModeText == y.Activity.ModeText).StartsOn, OrderByDirection.Descending },
+
+                    // finally, sort by title alphabetically
                     { x => x.Values.FirstOrDefault(y => y.ModeText == y.Activity.ModeText).Title, OrderByDirection.Ascending },
                 };
 
