37c351a0e5b440ecf55e180d8ff5d338a155b796
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2013-10-19 09:12:31 -0400

    Rename agreements/ApiModels.d.ts step 2.

---
+++ UCosmic.Web.Mvc/scripts/viewmodels/agreements/ApiModels.d.ts
@@ -0,0 +1,57 @@
+/// <reference path="../places/ApiModels.d.ts" />
+
+declare module Agreements.ApiModels {
+
+    export interface FlatEstablishment {
+        id: number;
+        officialName: string;
+        translatedName: string;
+        officialUrl: string;
+        countryName: string;
+        countryCode: string;
+        uCosmicCode: string;
+        ceebCode: string;
+    }
+
+    export interface ScalarEstablishment {
+        id: number;
+        typeId: number;
+        uCosmicCode: string;
+        ceebCode: string;
+    }
+
+    export interface NameInput {
+        id: number;
+        ownerId: number;
+        text: string;
+        isOfficialName: boolean;
+        isFormerName: boolean;
+        languageCode: string;
+    }
+
+    export interface Name extends NameInput {
+        languageName: string;
+    }
+
+    export interface Url {
+        id: number;
+        ownerId: number;
+        value: string;
+        isOfficialUrl: boolean;
+        isFormerUrl: boolean;
+    }
+
+    export interface Location {
+        center: Places.ApiModels.Point;
+        box: Places.ApiModels.Box;
+        googleMapZoomLevel?: number;
+        places: Places.ApiModels.Place[];
+    }
+
+    export interface PutLocation {
+        center?: Places.ApiModels.Point;
+        box?: Places.ApiModels.Box;
+        googleMapZoomLevel?: number;
+        placeId?: number;
+    }
+}
