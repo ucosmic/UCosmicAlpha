e05894c93ddfffa0c094136f52cedae4294ed7bc
danludwig <Dan.Ludwig@ucosmic.org>
2014-02-07 11:45:04 -0500

    Merge work task for fixing USF email addresses from topic branch into work.

---
+++ UCosmic.Domain/Domain/People/Work/FixUsfEmailAddresses.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace UCosmic.Domain.People
+{
+    public class FixUsfEmailAddresses : IDefineWork
+    {
+        public TimeSpan Interval { get { return TimeSpan.MaxValue; } }
+    }
+
+    public class PerformFixUsfEmailAddressesWork : IPerformWork<FixUsfEmailAddresses>
+    {
+        private readonly ICommandEntities _entities;
+
+        public PerformFixUsfEmailAddressesWork(ICommandEntities entities)
+        {
+            _entities = entities;
+        }
+
+        public void Perform(FixUsfEmailAddresses job)
+        {
+            var peopleWithoutEmailAddress = _entities.Get<Person>()
+                .EagerLoad(_entities, new Expression<Func<Person, object>>[]
+                {
+                    x => x.User,
+                })
+                .Where(x => x.User != null && x.User.Name.EndsWith("@usf.edu") && !x.Emails.Any())
+                .ToArray()
+            ;
+
+            if (peopleWithoutEmailAddress.Length < 1) return;
+
+            foreach (var person in peopleWithoutEmailAddress)
+            {
+                var emailValue = person.User.Name;
+                if (person.User.Name.Equals("ccao@usf.edu"))
+                {
+                    emailValue = "ccao@health.usf.edu";
+                }
+                person.Emails.Add(new EmailAddress
+                {
+                    IsConfirmed = true,
+                    IsDefault = true,
+                    Number = 1,
+                    Value = emailValue,
+                });
+            }
+
+            _entities.SaveChanges();
+        }
+    }
+}
