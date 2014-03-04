c6422c5a9dc9a33f373819ff7b249a5f23405881
timbow574521 <timtwillis@gmail.com>
2014-03-04 08:23:53 -0500

    Update agreement settings viewmodel and view.

--- UCosmic.Web.Mvc/scripts/viewmodels/agreements/settings.js
+++ UCosmic.Web.Mvc/scripts/viewmodels/agreements/settings.js
@@ -1,12 +1,60 @@
-var Agreements;
+﻿var Agreements;
 (function (Agreements) {
     (function (ViewModels) {
-        var settings = (function () {
-            function settings() {
+        var Settings = (function () {
+            function Settings() {
+                this.isCustomTypeAllowed = ko.observable();
+                this.isCustomStatusAllowed = ko.observable();
+                this.isCustomContactTypeAllowed = ko.observable();
+                this.statusOptions = ko.mapping.fromJS([]);
+                this.contactTypeOptions = ko.mapping.fromJS([]);
+                this.typeOptions = ko.mapping.fromJS([]);
             }
-            return settings;
+            Settings.prototype.processSettings = function (result) {
+                var self = this;
+
+                this.isCustomTypeAllowed(result.isCustomTypeAllowed);
+                this.isCustomStatusAllowed(result.isCustomStatusAllowed);
+                this.isCustomContactTypeAllowed(result.isCustomContactTypeAllowed);
+                this.statusOptions.push(new Agreements.SelectConstructor("", ""));
+                for (var i = 0, j = result.statusOptions.length; i < j; i++) {
+                    this.statusOptions.push(new Agreements.SelectConstructor(result.statusOptions[i], result.statusOptions[i]));
+                }
+                ;
+                this.contactTypeOptions.push(new Agreements.SelectConstructor("", undefined));
+                for (var i = 0, j = result.contactTypeOptions.length; i < j; i++) {
+                    this.contactTypeOptions.push(new Agreements.SelectConstructor(result.contactTypeOptions[i], result.contactTypeOptions[i]));
+                }
+                ;
+                this.typeOptions.push(new Agreements.SelectConstructor("", ""));
+                for (var i = 0, j = result.typeOptions.length; i < j; i++) {
+                    this.typeOptions.push(new Agreements.SelectConstructor(result.typeOptions[i], result.typeOptions[i]));
+                }
+                ;
+            };
+
+            Settings.prototype._getSettings = function () {
+                var _this = this;
+                var url = 'App.Routes.WebApi.Agreements.Settings.get()', agreementSettingsGet;
+
+                $.ajax({
+                    url: eval(url),
+                    type: 'GET'
+                }).done(function (result) {
+                    _this.processSettings(result);
+                }).fail(function (xhr) {
+                    App.Failures.message(xhr, xhr.responseText, true);
+                });
+            };
+
+            Settings.prototype.removeTypeOption = function (me, e) {
+                this.typeOptions.remove(me);
+                e.preventDefault();
+                e.stopPropagation();
+            };
+            return Settings;
         })();
-        ViewModels.settings = settings;
+        ViewModels.Settings = Settings;
     })(Agreements.ViewModels || (Agreements.ViewModels = {}));
     var ViewModels = Agreements.ViewModels;
 })(Agreements || (Agreements = {}));
--- UCosmic.Web.Mvc/scripts/viewmodels/agreements/settings.ts
+++ UCosmic.Web.Mvc/scripts/viewmodels/agreements/settings.ts
@@ -1,6 +1,71 @@
  module Agreements.ViewModels {
-     export class settings {
+     export class Settings {
          
+         constructor() {
+             
+         }
+
+         isCustomTypeAllowed = ko.observable();
+         isCustomStatusAllowed = ko.observable();
+         isCustomContactTypeAllowed = ko.observable();
+         statusOptions = ko.mapping.fromJS([]);
+         contactTypeOptions = ko.mapping.fromJS([]);
+         typeOptions = ko.mapping.fromJS([]);
+
+         private processSettings(result): void {
+             var self = this;
+
+             this.isCustomTypeAllowed(result.isCustomTypeAllowed);
+             this.isCustomStatusAllowed(result.isCustomStatusAllowed);
+             this.isCustomContactTypeAllowed(result.isCustomContactTypeAllowed);
+             this.statusOptions.push(new Agreements.SelectConstructor("", ""));
+             for (var i = 0, j = result.statusOptions.length; i < j; i++) {
+                 this.statusOptions.push(new Agreements.SelectConstructor(result.statusOptions[i], result.statusOptions[i]));
+             };
+             this.contactTypeOptions.push(new Agreements.SelectConstructor("", undefined));
+             for (var i = 0, j = result.contactTypeOptions.length; i < j; i++) {
+                 this.contactTypeOptions.push(new Agreements.SelectConstructor(result.contactTypeOptions[i], result.contactTypeOptions[i]));
+             };
+             this.typeOptions.push(new Agreements.SelectConstructor("", ""));
+             for (var i = 0, j = result.typeOptions.length; i < j; i++) {
+                 this.typeOptions.push(new Agreements.SelectConstructor(result.typeOptions[i], result.typeOptions[i]));
+             };
+         }
+         //get settings for agreements.
+         private _getSettings(): void {
+             var url = 'App.Routes.WebApi.Agreements.Settings.get()',
+                 agreementSettingsGet;
+
+             $.ajax({
+                 url: eval(url),
+                 type: 'GET'
+             })
+                 .done((result) => {
+                     this.processSettings(result);
+                 })
+                 .fail(function (xhr) {
+                     App.Failures.message(xhr, xhr.responseText, true);
+                 });
+         }
+
+         removeTypeOption(me, e): void {
+             //if (this.agreementId !== 0) {
+             //    this.deletedPhones.push(me.id);
+             //    //var url = App.Routes.WebApi.Agreements.Contacts.Phones.del(this.agreementId, me.contactId, me.id);
+
+             //    //$.ajax({
+             //    //    url: url,
+             //    //    type: 'DELETE',
+             //    //    success: (): void => {
+             //    //        this.contactPhones.remove(me);
+             //    //        $("body").css("min-height", ($(window).height() + $("body").height() - ($(window).height() * 1.1)));
+             //    //    }
+             //    //});
+             //}
+             this.typeOptions.remove(me);
+             e.preventDefault();
+             e.stopPropagation();
+         }
      }
 
  }
