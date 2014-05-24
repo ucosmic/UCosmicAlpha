1e142b0833364fb176e18f13bb08cdba47024d98
timbow574521 <timtwillis@gmail.com>
2014-05-24 09:52:59 -0400

    Update activitysearchmapModel for faculty and staff map.

--- UCosmic.Web.Mvc/Models/Activities/Search/ActivitySearchMapModel.cs
+++ UCosmic.Web.Mvc/Models/Activities/Search/ActivitySearchMapModel.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace UCosmic.Web.Mvc.Models
 {
     public class ActivitySearchMapModel
     {
+        //public ActivitySearchResultMapModel[] Output { get; set; }
         public string Domain { get; set; }
         public ActivitySearchInputModel Input { get; set; }
-        public PageOfActivitySearchResultMapModel Output { get; set; }
+        //public IEnumerable<ActivitySearchMapPlaceModel> Continents { get; set; }
+        //public PageOfActivitySearchResultMapModel Output { get; set; }
         public IEnumerable<ActivityTypeModel> ActivityTypes { get; set; }
     }
 }
