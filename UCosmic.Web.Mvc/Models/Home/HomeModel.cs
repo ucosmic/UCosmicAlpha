0d3fd78853beec9b54c51c7cd8cfc0691ffdca45
timbow574521 <timtwillis@gmail.com>
2014-08-10 09:41:49 -0400

    Create mvc HomeModel.cs for customizable home page.

---
+++ UCosmic.Web.Mvc/Models/Home/HomeModel.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using UCosmic.Domain.Employees;
+
+namespace UCosmic.Web.Mvc.Models
+{
+    public class FacultyRankApiModel
+    {
+        public int Id { get; set; }
+        public string Rank { get; set; }
+    }
+
+    public static class FacultyRankProfiler
+    {
+        public class EntityToModelProfile: Profile
+        {
+            protected override void Configure()
+            {
+                CreateMap<EmployeeFacultyRank, FacultyRankApiModel>();
+            }
+        }
+    }
+}
