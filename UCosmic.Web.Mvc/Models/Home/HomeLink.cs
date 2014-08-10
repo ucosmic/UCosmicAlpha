22569452fb79fc44ffd07ef347d118b8e13504c2
timbow574521 <timtwillis@gmail.com>
2014-08-10 09:33:45 -0400

    Create homelinks.cs mvc modle for customizable home page.

---
+++ UCosmic.Web.Mvc/Models/Home/HomeLink.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using UCosmic.Domain.Home;
+
+namespace UCosmic.Web.Mvc.Models
+{
+    public class HomeLinkModel
+    {
+        public int Id { get; set; }
+        public int HomeSectionId { get; set; }
+        public string Url { get; set; }
+        public string Text { get; set; }
+    }
+
+    public static class HomeLinkProfiler
+    {
+        public class EntityToModelProfile: Profile
+        {
+            protected override void Configure()
+            {
+                CreateMap<HomeLink, HomeLink Model>();
+            }
+        }
+    }
+}
