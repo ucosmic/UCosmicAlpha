9dede3f9bfeeb4496e50e8cd55c40df62c002703
timbow574521 <timtwillis@gmail.com>
2014-08-10 09:31:05 -0400

    Create homesection.cs mvc modle for customizable home page.

---
+++ UCosmic.Web.Mvc/Models/Home/HomeSection.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using UCosmic.Domain.Home;
+using UCosmic.Domain.Files;
+using UCosmic.Web.Mvc.Models;
+
+namespace UCosmic.Web.Mvc.Models
+{
+    public class HomeSectionModel
+    {
+        public int Id { get; set; }
+        public int EstablishmentId { get; set; }
+        public ExternalFile Photo { get; set; }
+        public HomeLink[] Links {get; set;}
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+    public static class HomeSectionProfiler
+    {
+        public class EntityToModelProfile: Profile
+        {
+            protected override void Configure()
+            {
+                CreateMap<HomeSection, HomeSectionModel>();
+            }
+        }
+    }
+}
