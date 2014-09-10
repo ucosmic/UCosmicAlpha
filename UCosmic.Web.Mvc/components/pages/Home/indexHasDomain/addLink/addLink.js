94fa03b8ce1c653fc424fa3d7febb55500be26f9
timbow574521 <timtwillis@gmail.com>
2014-09-10 14:10:06 -0400

    Create component home addlink.ts.

---
+++ UCosmic.Web.Mvc/components/pages/Home/indexHasDomain/addLink/addLink.js
@@ -0,0 +1,14 @@
+﻿Polymer('polymer-content-home-has-domain-edit-add-link', {
+    linkAdded: { url: "", text: "" },
+    addedLink: false,
+    addLink: function () {
+        if (this.$.linkText.inputChange() && this.$.url.inputChange()) {
+            this.linkAdded.url = this.url;
+            this.linkAdded.text = this.linkText;
+            this.addedLink = true;
+        } else {
+            this.$.linkText.inputChange();
+            this.$.url.inputChange();
+        }
+    }
+});
