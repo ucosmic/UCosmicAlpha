06f0f4735d26a97b7f78ea28f5bab234d18e9d78
timbow574521 <timtwillis@gmail.com>
2014-09-10 13:56:51 -0400

    Create component home index.ts.

---
+++ UCosmic.Web.Mvc/components/pages/Home/index.js
@@ -0,0 +1,14 @@
+﻿Polymer('polymer-content-home-index', {
+    ready: function () {
+        if (this.notification) {
+            var polymerNotification = document.createElement('polymer-notification');
+            polymerNotification.message = "testing";
+            polymerNotification.type = 'notify';
+            polymerNotification.fadeOutDelay = '10000';
+            polymerNotification.bindToElement = $("header > .container > .content");
+
+            polymerNotification.setAttribute('id', 'myAlert' + Date.now());
+            document.body.appendChild(polymerNotification);
+        }
+    }
+});
---
+++ UCosmic.Web.Mvc/components/pages/Home/index.ts
@@ -0,0 +1,16 @@
+﻿/// <reference path="../../typediff/mytypes.d.ts" />
+
+Polymer('polymer-content-home-index', {
+    ready: function () {
+        if (this.notification) {
+            var polymerNotification = document.createElement('polymer-notification');
+            polymerNotification.message = "testing";
+            polymerNotification.type = 'notify';
+            polymerNotification.fadeOutDelay = '10000';
+            polymerNotification.bindToElement = $("header > .container > .content");
+            //polymerNotification.bindToElement = $("header > .container > .content");
+            polymerNotification.setAttribute('id', 'myAlert' + Date.now());
+            document.body.appendChild(polymerNotification);
+        }
+    },
+}); 
