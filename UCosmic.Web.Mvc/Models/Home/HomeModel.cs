758f34c2fa40552b824a51a3cd907e6e1145e4f5
timbow574521 <timtwillis@gmail.com>
2014-09-10 13:34:10 -0400

    Create model Home homesectionmodel.cs.

--- UCosmic.Web.Mvc/Models/Home/HomeModel.cs
+++ UCosmic.Web.Mvc/Models/Home/HomeModel.cs
@@ -3,20 +3,20 @@ using UCosmic.Domain.Employees;
 
 namespace UCosmic.Web.Mvc.Models
 {
-    public class FacultyRankApiModel
-    {
-        public int Id { get; set; }
-        public string Rank { get; set; }
-    }
+    //public class FacultyRankApiModel
+    //{
+    //    public int Id { get; set; }
+    //    public string Rank { get; set; }
+    //}
 
-    public static class FacultyRankProfiler
-    {
-        public class EntityToModelProfile: Profile
-        {
-            protected override void Configure()
-            {
-                CreateMap<EmployeeFacultyRank, FacultyRankApiModel>();
-            }
-        }
-    }
+    //public static class FacultyRankProfiler
+    //{
+    //    public class EntityToModelProfile: Profile
+    //    {
+    //        protected override void Configure()
+    //        {
+    //            CreateMap<EmployeeFacultyRank, FacultyRankApiModel>();
+    //        }
+    //    }
+    //}
 }
