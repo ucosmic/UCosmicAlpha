dd156ce65d2b40536213d84dd8272656f76eaec9
timbow574521 <timtwillis@gmail.com>
2014-09-10 13:10:19 -0400

    Create validation homesectionvalidation.cs.

---
+++ UCosmic.Domain/Domain/Home/Validation/HomeSectionValidation.cs
@@ -0,0 +1,11 @@
+﻿namespace UCosmic.Domain.Home
+{
+    public static class MustHaveTitle
+    {
+        public const string FailMessage = "Title is required.";
+    }
+    public static class MustHaveDescription
+    {
+        public const string FailMessage = "Description is required.";
+    }
+}
