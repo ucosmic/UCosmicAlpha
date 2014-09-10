813a2ef07158027bdac4729064dbf5903608696f
timbow574521 <timtwillis@gmail.com>
2014-09-10 13:03:03 -0400

    Create query HomeAlertById.cs.

---
+++ UCosmic.Domain/Domain/Home/Queries/HomeAlertById.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace UCosmic.Domain.Home
+{
+    public class HomeAlertById : BaseEntityQuery<HomeAlert>, IDefineQuery<HomeAlert>
+    {
+        public HomeAlertById(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; private set; }
+    }
+
+    public class HandleHomeAlertByIdQuery : IHandleQueries<HomeAlertById, HomeAlert>
+    {
+        private readonly IQueryEntities _entities;
+
+        public HandleHomeAlertByIdQuery(IQueryEntities entities)
+        {
+            _entities = entities;
+        }
+
+        public HomeAlert Handle(HomeAlertById query)
+        {
+            if (query == null) throw new ArgumentNullException("query");
+
+            var result = _entities.Query<HomeAlert>()
+                .EagerLoad(_entities, query.EagerLoad)
+                .ById(query.Id)
+                //.SingleOrDefault(x => x.Id == query.Id)
+            ;
+
+            return result;
+        }
+    }
+}
