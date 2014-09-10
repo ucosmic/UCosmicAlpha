5b2e02c037490224570bd46d5343879e02d41e2d
timbow574521 <timtwillis@gmail.com>
2014-09-10 13:15:21 -0400

    Create home internal queryhomesection.cs.

---
+++ UCosmic.Domain/Domain/Home/_Internal/QueryHomeSection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace UCosmic.Domain.Home
+{
+    internal static class QueryHomeSection
+    {
+
+        internal static HomeSection ById(this IQueryable<HomeSection> queryable, Int32 id, bool allowNull = true)
+        {
+            return allowNull
+                ? queryable.SingleOrDefault(ById(id))
+                : queryable.Single(ById(id));
+        }
+        private static Expression<Func<HomeSection, bool>> ById(Int32 id)
+        {
+            return x => x.Id != null && x.Id == id;
+        }
+        
+
+    }
+}
