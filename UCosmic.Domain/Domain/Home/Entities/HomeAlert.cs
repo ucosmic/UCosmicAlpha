2be70aa2d474d5d9ac2733d4731150fb163ace09
timbow574521 <timtwillis@gmail.com>
2014-09-10 12:58:52 -0400

    Create Entity HomeAlert.cs.

---
+++ UCosmic.Domain/Domain/Home/Entities/HomeAlert.cs
@@ -0,0 +1,12 @@
+﻿
+using UCosmic.Domain.Establishments;
+namespace UCosmic.Domain.Home
+{
+    public class HomeAlert : Entity
+    {
+        public int Id { get; set; }
+        public int? EstablishmentId { get; set; }
+        public virtual Establishment Establishment { get; protected internal set; }
+        public string Text { get; set; }
+    }
+}
