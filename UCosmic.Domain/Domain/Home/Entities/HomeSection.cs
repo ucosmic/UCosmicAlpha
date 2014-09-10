25cd1ee06a3e058ed9b0d471c91123c321f98b6e
timbow574521 <timtwillis@gmail.com>
2014-09-10 13:00:04 -0400

    Create entity homeSection.cs.

---
+++ UCosmic.Domain/Domain/Home/Entities/HomeSection.cs
@@ -0,0 +1,24 @@
+﻿using UCosmic.Domain.Files;
+using UCosmic.Domain.Establishments;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+namespace UCosmic.Domain.Home
+{
+    public class HomeSection : Entity
+    {
+
+        protected internal HomeSection()
+        {
+            Links = new Collection<HomeLink>();
+        }
+        internal const string PhotoPathFormat = "/home-photos/{0}/{1}";
+        public int Id { get; set; }
+        public int? EstablishmentId { get; set; }
+        public virtual Establishment Establishment { get; protected internal set; }
+        public virtual ExternalFile Photo { get; protected internal set; }
+        //public HomeLink[] Links { get; set; }
+        public virtual ICollection<HomeLink> Links { get; protected set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
