b9e2e2f595b361fee817f4bbe56e54321600a6f3
timbow574521 <timtwillis@gmail.com>
2014-09-10 13:01:17 -0400

    Create Query HomeAlertByEstablishmentId.cs.

---
+++ UCosmic.Domain/Domain/Home/Queries/HomeAlertByEstablishmentId.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Security.Principal;
+
+namespace UCosmic.Domain.Home
+{
+    public class HomeAlertByEstablishmentId : BaseEntityQuery<HomeAlert>, IDefineQuery<HomeAlert[]>
+    {
+        public HomeAlertByEstablishmentId(int establishmentId)
+        {
+            EstablishmentId = establishmentId;
+        }
+
+        public int EstablishmentId { get; private set; }
+    }
+
+    public class HandleHomeAlertByEstablishmentIdQuery : IHandleQueries<HomeAlertByEstablishmentId, HomeAlert[]>
+    {
+        private readonly IQueryEntities _entities;
+
+        public HandleHomeAlertByEstablishmentIdQuery(IQueryEntities entities)
+        {
+            _entities = entities;
+        }
+
+        public HomeAlert[] Handle(HomeAlertByEstablishmentId query)
+        {
+            if (query == null) throw new ArgumentNullException("query");
+
+            //var result = _entities.Query<HomeAlert>()
+            //    .EagerLoad(_entities, query.EagerLoad)
+            //    .ByEstablishmentId(query.EstablishmentId)
+            //    //.SingleOrDefault(x => x.Id == query.Id)
+            //;
+            IQueryable<HomeAlert> results = _entities.Query<HomeAlert>()
+                                                             .Where(a => a.EstablishmentId == query.EstablishmentId);
+                                                             //.OrderBy(a => (a.LanguageId != null) ?
+                                                             //    a.Language.Names.FirstOrDefault().Text :
+                                                             //    a.Other);
+
+            return results.ToArray();
+        }
+    }
+
+}
