d132f1cd8a008314e3ec9dac00fe465937e42c42
timbow574521 <timtwillis@gmail.com>
2014-09-10 13:14:40 -0400

    Create home internal queryhomealert.cs.

---
+++ UCosmic.Domain/Domain/Home/_Internal/QueryHomeAlert.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace UCosmic.Domain.Home
+{
+    internal static class QueryHomeAlert
+    {
+
+        internal static HomeAlert ById(this IQueryable<HomeAlert> queryable, Int32 id, bool allowNull = true)
+        {
+            return allowNull
+                ? queryable.SingleOrDefault(ById(id))
+                : queryable.Single(ById(id));
+        }
+        private static Expression<Func<HomeAlert, bool>> ById(Int32 id)
+        {
+            return x => x.Id != null && x.Id == id;
+        }
+        
+
+    }
+}
