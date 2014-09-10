5c4535456fb2d3328c074d710cc6a1ff75e86e22
timbow574521 <timtwillis@gmail.com>
2014-09-10 13:05:18 -0400

    Create query homesectionbyid.cs.

---
+++ UCosmic.Domain/Domain/Home/Queries/HomeSectionById.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace UCosmic.Domain.Home
+{
+    public class HomeSectionById : BaseEntityQuery<HomeSection>, IDefineQuery<HomeSection>
+    {
+        public HomeSectionById(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; private set; }
+    }
+
+    public class HandleHomeSectionByIdQuery : IHandleQueries<HomeSectionById, HomeSection>
+    {
+        private readonly IQueryEntities _entities;
+
+        public HandleHomeSectionByIdQuery(IQueryEntities entities)
+        {
+            _entities = entities;
+        }
+
+        public HomeSection Handle(HomeSectionById query)
+        {
+            if (query == null) throw new ArgumentNullException("query");
+
+            var result = _entities.Query<HomeSection>()
+                .EagerLoad(_entities, query.EagerLoad)
+                .ById(query.Id)
+                //.SingleOrDefault(x => x.Id == query.Id)
+            ;
+
+            return result;
+        }
+    }
+}
