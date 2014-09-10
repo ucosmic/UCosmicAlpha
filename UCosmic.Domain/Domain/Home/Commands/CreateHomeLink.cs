8ed59090770261f37cab4df53e5fb7cef7a6cf9d
timbow574521 <timtwillis@gmail.com>
2014-09-10 12:50:45 -0400

    Create command createhomelink.cs.

---
+++ UCosmic.Domain/Domain/Home/Commands/CreateHomeLink.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+using System.Security.Principal;
+using FluentValidation;
+using Newtonsoft.Json;
+using UCosmic.Domain.Audit;
+using UCosmic.Domain.Home;
+using UCosmic.Domain.Identity;
+
+namespace UCosmic.Domain.Home
+{
+    public class CreateHomeLink
+    {
+        public CreateHomeLink() { }
+
+        public CreateHomeLink(IPrincipal principal, int homeSectionId, HomeLink homeLink)
+        {
+            if (principal == null) throw new ArgumentNullException("principal");
+            Principal = principal;
+            HomeSectionId = homeSectionId;
+            HomeLink = homeLink;
+        }
+
+        //internal CreateHomeLink(IPrincipal principal, int homeSectionId, HomeLink homeLink)
+        //{
+        //    if (principal == null) throw new ArgumentNullException("principal");
+        //    Principal = principal;
+        //    HomeSectionId = homeSection;
+        //    HomeLink = homeLink;
+        //}
+        internal HomeLink HomeLink { get; private set; }
+        public IPrincipal Principal { get; private set; }
+        public int HomeSectionId { get; private set; }
+        internal bool NoCommit { get; set; }
+    }
+
+    public class ValidateCreateHomeLinkCommand : AbstractValidator<CreateHomeLink>
+    {
+        public ValidateCreateHomeLinkCommand(IProcessQueries queryProcessor)
+        {
+            CascadeMode = CascadeMode.StopOnFirstFailure;
+
+            // principal must be authorized to perform this action
+            RuleFor(x => x.Principal)
+                .NotNull()
+                    .WithMessage(MustNotHaveNullPrincipal.FailMessage)
+                .MustNotHaveEmptyIdentityName()
+                    .WithMessage(MustNotHaveEmptyIdentityName.FailMessage)
+                .MustBeInAnyRole(RoleName.SecurityAdministrator)
+            ;
+
+            //RuleFor(x => x.HomeSection)
+            //    .Must((command, homeSection) => homeSection.HomeLinks.All(x => x.EstablishmentId != command.EstablishmentId))
+            //        .WithMessage(MustNotOwnHomeLinkWithId<object>.FailMessageNewFormat, x => x.EstablishmentId)
+            //;
+
+            // establishment id must exist in database
+            //RuleFor(x => x.EstablishmentId)
+            //    .MustFindEstablishmentById(queryProcessor)
+            //        .WithMessage(MustFindEstablishmentById.FailMessageFormat, x => x.EstablishmentId);
+        }
+    }
+
+    public class HandleCreateHomeLinkCommand : IHandleCommands<CreateHomeLink>
+    {
+        private readonly ICommandEntities _entities;
+        private readonly IProcessQueries _queryProcessor;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HandleCreateHomeLinkCommand(ICommandEntities entities
+            , IProcessQueries queryProcessor
+            , IUnitOfWork unitOfWork
+        )
+        {
+            _entities = entities;
+            _queryProcessor = queryProcessor;
+            _unitOfWork = unitOfWork;
+        }
+
+        public void Handle(CreateHomeLink command)
+        {
+            if (command == null) throw new ArgumentNullException("command");
+
+            var ownedTenantIds = _queryProcessor.Execute(new MyOwnedTenantIds(command.Principal));
+
+            var entity = new HomeLink
+            {
+                Url = command.HomeLink.Url,
+                Text = command.HomeLink.Text,
+                HomeSectionId = command.HomeLink.HomeSectionId,
+            };
+            
+            _entities.Create(entity);
+            if (!command.NoCommit) _unitOfWork.SaveChanges();
+        }
+    }
+}
