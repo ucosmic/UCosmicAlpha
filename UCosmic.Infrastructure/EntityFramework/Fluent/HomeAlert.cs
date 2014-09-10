35c260777e54d515551a3a0bba632c4306d1d863
timbow574521 <timtwillis@gmail.com>
2014-09-10 13:23:20 -0400

    Create fluent homealert.cs.

---
+++ UCosmic.Infrastructure/EntityFramework/Fluent/HomeAlert.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using UCosmic.Domain.Home;
+
+namespace UCosmic.EntityFramework
+{
+    public class HomeAlertOrm : EntityTypeConfiguration<HomeAlert>
+    {
+        public HomeAlertOrm()
+        {
+            ToTable(typeof(HomeAlert).Name, DbSchemaName.HomeAlert);
+
+            // has one establishment
+            HasOptional(d => d.Establishment)
+                .WithMany()
+                .HasForeignKey(d => d.EstablishmentId)
+                .WillCascadeOnDelete();
+
+            Property(p => p.Text).IsRequired();
+        }
+    }
+    
+}
