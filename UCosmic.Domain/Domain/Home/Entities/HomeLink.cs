2f0397e62a16f581eaff4a2e4639ffa13b34ec94
timbow574521 <timtwillis@gmail.com>
2014-09-10 12:59:35 -0400

    Create entity homelink.cs.

---
+++ UCosmic.Domain/Domain/Home/Entities/HomeLink.cs
@@ -0,0 +1,11 @@
+﻿namespace UCosmic.Domain.Home
+{
+    public class HomeLink : Entity
+    {
+        public int Id { get; set; }
+        public int HomeSectionId { get; protected internal set; }
+        public virtual HomeSection HomeSection { get; protected internal set; }
+        public string Url { get; set; }
+        public string Text { get; set; }
+    }
+}
