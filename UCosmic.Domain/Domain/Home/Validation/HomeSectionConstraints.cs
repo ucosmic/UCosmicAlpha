2c47ae6a9f2b226f3f9e94132aa3bc9cab5ff7c0
timbow574521 <timtwillis@gmail.com>
2014-09-10 13:06:41 -0400

    Create validation homesectionconstraints.cs.

---
+++ UCosmic.Domain/Domain/Home/Validation/HomeSectionConstraints.cs
@@ -0,0 +1,14 @@
+﻿namespace UCosmic.Domain.Home
+{
+    public static class HomeSectionConstraints
+    {
+
+        public const int TitleMaxLength = 100;
+        public const int DescriptionMaxLength = 50;
+        public static readonly string[] AllowedPhotoFileExtensions = new[]
+        {
+            "png", "jpg", "jpeg", "gif",
+        };
+    }
+
+}
