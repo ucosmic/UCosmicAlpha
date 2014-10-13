72df9f4dfc7230374a510dd76324d191fa0f883e
timbow574521 <timtwillis@gmail.com>
2014-10-12 22:45:44 -0400

    Create Home alert api model.

--- UCosmic.Web.Mvc/Models/Employees/EmployeesBibNavModel.cs
+++ UCosmic.Web.Mvc/Models/Employees/EmployeesBibNavModel.cs
@@ -6,6 +6,7 @@ namespace UCosmic.Web.Mvc.Models
     {
         public bool Activities { get; set; }
         public bool Degrees { get; set; }
+        public bool Languages { get; set; }
         public bool FindAnExpert { get; set; }
         public bool GeographicExpertise { get; set; }
         public bool LanguageExpertise { get; set; }
