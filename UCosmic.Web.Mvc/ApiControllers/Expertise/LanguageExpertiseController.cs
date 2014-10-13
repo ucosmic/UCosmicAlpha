a81e0a77e6944a549a1a0a1d752859082487db90
timbow574521 <timtwillis@gmail.com>
2014-10-12 22:31:23 -0400

    Create Home Alert Controller.

--- UCosmic.Web.Mvc/ApiControllers/Expertise/LanguageExpertiseController.cs
+++ UCosmic.Web.Mvc/ApiControllers/Expertise/LanguageExpertiseController.cs
@@ -9,7 +9,7 @@ using System.Web.Http;
 using AttributeRouting;
 using AttributeRouting.Web.Http;
 using AutoMapper;
-using UCosmic.Domain.LanguageExpertise;
+using UCosmic.Domain.LanguageExpertises;
 using UCosmic.Web.Mvc.Models;
 
 namespace UCosmic.Web.Mvc.ApiControllers
---
+++ UCosmic.Web.Mvc/ApiControllers/Home/HomeAlertController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using AttributeRouting;
+using AttributeRouting.Web.Http;
+using FluentValidation;
+using FluentValidation.Results;
+using Newtonsoft.Json;
+using UCosmic.Domain.People;
+using UCosmic.Domain.Home;
+using UCosmic.Web.Mvc.Models;
+using AutoMapper;
+using System.Diagnostics;
+
+namespace UCosmic.Web.Mvc.ApiControllers
+{
+    [System.Web.Http.Authorize]
+    [RoutePrefix("api/home/alert")]
+    public class HomeAlertController : ApiController
+    {
+        private readonly IProcessQueries _queryProcessor;
+        private readonly IHandleCommands<UpdateHomeAlert> _alertUpdateHandler;
+        private readonly IValidator<UpdateHomeAlert> _alertUpdateValidator;
+        private readonly IHandleCommands<DeleteHomeAlert> _alertDeleteHandler;
+        private readonly IHandleCommands<CreateHomeAlert> _alertCreateHandler;
+
+        public HomeAlertController(IProcessQueries queryProcessor
+            , IValidator<UpdateHomeAlert> alertUpdateValidator
+            , IHandleCommands<UpdateHomeAlert> alertUpdateHandler
+            , IHandleCommands<DeleteHomeAlert> alertDeleteHandler
+            , IHandleCommands<CreateHomeAlert> alertCreateHandler
+        )
+        {
+            _queryProcessor = queryProcessor;
+            _alertUpdateValidator = alertUpdateValidator;
+            _alertUpdateHandler = alertUpdateHandler;
+            _alertDeleteHandler = alertDeleteHandler;
+            _alertCreateHandler = alertCreateHandler;
+        }
+
+        [GET("")]
+        [CacheHttpGet(Duration = 3600)]
+        public HttpResponseMessage Get(int establishmentId)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Your alert was retrieved successfully.");
+        }
+
+        [POST("")]
+        //public HttpResponseMessage Post(string homeAlert, string test, string test2)
+        //public HttpResponseMessage Post(string homeAlert)
+        public HttpResponseMessage Post(HomeAlertApiModel homeAlert)
+        {
+
+            var person = _queryProcessor.Execute(new MyPerson(User));
+            var establishmentId = person.Affiliations.First(x => x.IsDefault).EstablishmentId;
+            var homeAlertNow = _queryProcessor.Execute(new HomeAlertByEstablishmentId(establishmentId));
+            homeAlert.EstablishmentId = establishmentId;
+
+            if (homeAlertNow != null)
+            {
+                _alertDeleteHandler.Handle(new DeleteHomeAlert(User, homeAlertNow.Id));
+            }
+
+            var command = new CreateHomeAlert(User)
+            {
+                Text = homeAlert.Text,
+                IsDisabled = homeAlert.IsDisabled,
+                EstablishmentId = homeAlert.EstablishmentId
+            };
+
+            //var command = new CreateHomeAlert(User);
+            //Mapper.Map(homeAlert, command);
+            _alertCreateHandler.Handle(command);
+
+            var response = Request.CreateResponse(HttpStatusCode.Created, "Home alert was successfully created.");
+            //var url = Url.Link(null, new
+            //{
+            //    controller = "Agreements",
+            //    action = "Get",
+            //    homeAlertId = command.CreatedHomeAlertId,
+            //});
+            //Debug.Assert(url != null);
+            response.Headers.Location = new Uri("http://ucosmic.com?alertid="+command.CreatedHomeAlertId);
+            return response;
+            
+            //return Request.CreateResponse(HttpStatusCode.OK, "Your alert was created successfully.");
+        }
+
+
+        [DELETE("")]
+        public HttpResponseMessage Delete(int homeAlertId)
+        {
+            //make it like delete agreement and create purgeHomeAlert class...
+            _alertDeleteHandler.Handle(new DeleteHomeAlert(User, homeAlertId));
+
+            return Request.CreateResponse(HttpStatusCode.OK, "Your alert was deleted successfully.");
+        }
+    }
+}
