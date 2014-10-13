7977a24534eac43670d51ca94cce979e98a811c3
timbow574521 <timtwillis@gmail.com>
2014-10-12 23:02:02 -0400

    Add flasher and updates to home index.ts.

--- UCosmic.Web.Mvc/components/pages/Home/index.js
+++ UCosmic.Web.Mvc/components/pages/Home/index.js
@@ -1,14 +1,29 @@
 ﻿Polymer('polymer-content-home-index', {
     ready: function () {
-        if (this.notification) {
+        if (!this.flasherisdisabled) {
             var polymerNotification = document.createElement('polymer-notification');
-            polymerNotification.message = "testing";
+            polymerNotification.message = this.flashertext;
             polymerNotification.type = 'notify';
             polymerNotification.fadeOutDelay = '10000';
             polymerNotification.bindToElement = $("header > .container > .content");
+            polymerNotification.style.fontSize = "1.25em";
+            polymerNotification.mustClose = true;
 
             polymerNotification.setAttribute('id', 'myAlert' + Date.now());
             document.body.appendChild(polymerNotification);
+        }
+
+        if (this.homeSections && !this.homeSections[0].title) {
+            var homeSections = JSON.parse(this.homeSections);
+            if (!homeSections.length) {
+                this.homeSections = [];
+                this.homeSections[0] = homeSections;
+                if (this.homeSections.length) {
+                    this.homeSections = null;
+                }
+            } else {
+                this.homeSections = homeSections;
+            }
         }
     }
 });
--- UCosmic.Web.Mvc/components/pages/Home/index.ts
+++ UCosmic.Web.Mvc/components/pages/Home/index.ts
@@ -2,15 +2,50 @@
 
 Polymer('polymer-content-home-index', {
     ready: function () {
-        if (this.notification) {
+        //if (this.notification) {
+        //    var polymerNotification = document.createElement('polymer-notification');
+        //    polymerNotification.message = "testing";
+        //    polymerNotification.type = 'notify';
+        //    polymerNotification.fadeOutDelay = '10000';
+        //    polymerNotification.bindToElement = $("header > .container > .content");
+        //    //polymerNotification.bindToElement = $("header > .container > .content");
+        //    polymerNotification.setAttribute('id', 'myAlert' + Date.now());
+        //    document.body.appendChild(polymerNotification);
+        //}
+        //if (!this.flasherisdisabled) {
+        //    var polymerNotification = document.createElement('polymer-notification');
+        //    polymerNotification.message = this.flashertext;
+        //    polymerNotification.type = 'notify';
+        //    polymerNotification.fadeOutDelay = '10000';
+        //    polymerNotification.bindToElement = $("header > .container > .content");
+        //    //polymerNotification.bindToElement = $("header > .container > .content");
+        //    polymerNotification.setAttribute('id', 'myAlert' + Date.now());
+        //    document.body.appendChild(polymerNotification);
+        //}
+        if (!this.flasherisdisabled) {
             var polymerNotification = document.createElement('polymer-notification');
-            polymerNotification.message = "testing";
+            polymerNotification.message = this.flashertext;
             polymerNotification.type = 'notify';
             polymerNotification.fadeOutDelay = '10000';
             polymerNotification.bindToElement = $("header > .container > .content");
+            polymerNotification.style.fontSize = "1.25em";
+            polymerNotification.mustClose = true;
             //polymerNotification.bindToElement = $("header > .container > .content");
             polymerNotification.setAttribute('id', 'myAlert' + Date.now());
             document.body.appendChild(polymerNotification);
+        }
+        
+        if (this.homeSections && !this.homeSections[0].title) {
+            var homeSections = JSON.parse(this.homeSections);
+            if (!homeSections.length) {
+                this.homeSections = [];
+                this.homeSections[0] = homeSections;
+                if (this.homeSections.length){
+                    this.homeSections = null;
+                } 
+            } else {
+                this.homeSections = homeSections;
+            }
         }
     },
 }); 
