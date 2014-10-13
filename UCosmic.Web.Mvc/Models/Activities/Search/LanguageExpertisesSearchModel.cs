128bfe3fca80b448477bd7b5c28eaba7d652a6d3
timbow574521 <timtwillis@gmail.com>
2014-10-12 22:42:32 -0400

    Create Language Expertises Search Model.

---
+++ UCosmic.Web.Mvc/Models/Activities/Search/LanguageExpertisesSearchModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using UCosmic.Domain.LanguageExpertises;
+
+namespace UCosmic.Web.Mvc.Models
+{
+    public class LanguageExpertiseSearchModel
+    {
+        public string Domain { get; set; }
+        public LanguageExpertisesSearchInputModel Input { get; set; }
+        //public PagedQueryResult<LanguageExpertise> Output { get; set; }
+        //public List<LanguageExpertise> Output2 { get; set; }
+        public PageOfLanguageExpertiseSearchResultModel Output { get; set; }
+        public IEnumerable<SelectListItem> LanguageOptions { get; set; }
+    }
+}
