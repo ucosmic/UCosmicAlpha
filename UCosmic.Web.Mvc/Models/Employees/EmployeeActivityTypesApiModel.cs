65d844cb99d3ac7d07fdc32b5e1ed39da56d1d50
timbow574521 <timtwillis@gmail.com>
2014-11-10 17:25:32 -0500

    Create EmployeeActivityTypes api model.cs.

---
+++ UCosmic.Web.Mvc/Models/Employees/EmployeeActivityTypesApiModel.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using UCosmic.Domain.Languages;
+
+namespace UCosmic.Web.Mvc.Models
+{
+    public class EmployeeActivityTypesApiResultModel
+    {
+        public string type { get; set; }
+    }
+
+}
