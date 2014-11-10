7319d6ab8a7d1618076b5c0ba0be15a4831eeec2
timbow574521 <timtwillis@gmail.com>
2014-11-10 17:24:42 -0500

    Create Activity MapCounts api model.cs.

---
+++ UCosmic.Web.Mvc/Models/Activities/ActivityMapCountsApiModel.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using UCosmic.Domain.Languages;
+
+namespace UCosmic.Web.Mvc.Models
+{
+    public class ActivityMapCountsApiQueryResultModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public float latitude { get; set; }
+        public float longitude { get; set; }
+        public bool isContinent { get; set; }
+        public bool isCountry { get; set; }
+        public bool isRegion { get; set; }
+        public bool isWater { get; set; }
+        public string code { get; set; }
+        public MapPointModel center
+        {
+            get{
+                return new MapPointModel { Latitude = latitude, Longitude = longitude };
+            }
+            private set
+            {
+                
+            }
+        }
+    }
+    public class ActivityMapCountsAllApiQueryResultModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public float latitude { get; set; }
+        public float longitude { get; set; }
+        public bool isContinent { get; set; }
+        public string code { get; set; }
+        public MapPointModel center
+        {
+            get
+            {
+                return new MapPointModel { Latitude = latitude, Longitude = longitude };
+            }
+            private set
+            {
+
+            }
+        }
+    }
+}
