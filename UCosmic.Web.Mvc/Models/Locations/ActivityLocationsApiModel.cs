84e3b22ebd7e4e36d8ababa2df385ac7df41d0ba
timbow574521 <timtwillis@gmail.com>
2014-11-10 17:27:09 -0500

    Create ActivityLocations Api Model.cs

---
+++ UCosmic.Web.Mvc/Models/Locations/ActivityLocationsApiModel.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using UCosmic.Domain.Languages;
+
+namespace UCosmic.Web.Mvc.Models
+{
+    public class ActivityLocationsApiQueryResultModel
+    {
+        public int id { get; set; }
+        public string officialName { get; set; }
+        public string type { get; set; }
+    }
+    public class ActivityLocationsApiModel
+    {
+        public int LocationCount { get; set; }
+        public int TypeCount { get; set; }
+        public string Type { get; set; }
+
+        //public ActivityLocationsApiModel(int locationCount, int typeCount, string type)//cannot use a constructor because it creates an error with the JSON return...
+        //{
+        //    LocationCount = locationCount;
+        //    TypeCount = typeCount;
+        //    Type = type;
+        //}
+    }
+}
