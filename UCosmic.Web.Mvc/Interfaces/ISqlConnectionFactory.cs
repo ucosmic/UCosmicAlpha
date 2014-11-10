fdbfbd52ff8b8ff038a055606308c0e34cc058d2
timbow574521 <timtwillis@gmail.com>
2014-11-10 17:19:38 -0500

    Create interface ISQLConnectionFactory to database results back to generic ILists.

---
+++ UCosmic.Web.Mvc/Interfaces/ISqlConnectionFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using System.Linq;
+
+namespace UCosmic.Interfaces
+{
+	
+		public enum DB
+		{
+			UCosmic
+		}
+
+		public interface ISqlConnectionFactory
+		{
+            IDbConnection GetConnection(DB database);
+            IList<x> SelectList<x>(IDbConnection connection, string sql, object arguments);
+            IList<x> SelectList<x>(DB database, string sql, object arguments);
+            IList<x> SelectList<x>(DB database, string sql);
+			IList<x> SelectList<x>(IDbConnection connection, string sql, object parameters, CommandType commandtype);
+			void Execute(IDbConnection connection, string sql, object parameters);
+			void Execute(IDbConnection connection, string sql, object parameters, CommandType commandType);
+			void Execute(DB database, string sql, object parameters);
+			void Execute(DB database, string sql, object parameters, CommandType commandType);
+
+		}
+}
