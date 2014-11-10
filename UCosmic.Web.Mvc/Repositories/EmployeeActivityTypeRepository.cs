917a33a1243aba740f7134c8dd9af82740a5cc0c
timbow574521 <timtwillis@gmail.com>
2014-11-10 17:29:16 -0500

    Create EmployeeActivitytype Repository.cs.

---
+++ UCosmic.Web.Mvc/Repositories/EmployeeActivityTypeRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+//using UCosmic.Exceptions;
+using UCosmic.Interfaces;
+using UCosmic.Factories;
+using System.Web.Security;
+using System;
+using UCosmic.Domain.Places;
+using UCosmic.Web.Mvc.Models;
+
+
+namespace UCosmic.Repositories
+{
+    public class EmployeeActivityTypesRepository// : ILocationsRepository
+	{
+
+        public IList<String> EmployeeActivityTypes_By_establishmentId(int? EstablishmentId)
+        {
+
+
+            SqlConnectionFactory connectionFactory = new SqlConnectionFactory();
+            const string sql = "select type  FROM [UCosmicTest].[employees].[employeeactivitytype] " +
+                  " where establishmentid=3306";
+            IList<String> activityLocations = connectionFactory.SelectList<String>(DB.UCosmic, sql);
+            
+            return activityLocations;
+        }
+        
+	}
+}
