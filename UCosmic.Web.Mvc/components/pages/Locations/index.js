745c82d42e544171e24c43b51b9fce6ee2f6e7f2
timbow574521 <timtwillis@gmail.com>
2014-11-10 17:40:49 -0500

    Create locations index.ts component for locations module.

---
+++ UCosmic.Web.Mvc/components/pages/Locations/index.js
@@ -0,0 +1,25 @@
+﻿Polymer('is-page-locations-index', {
+    isAjaxing: false,
+    ready: function () {
+    },
+    countriesResponse: function (response) {
+        this.isAjaxing = false;
+
+        if (!response.detail.response.error) {
+            console.log(response.detail.response);
+            this.countries = response.detail.response;
+        } else {
+            console.log(response.detail.response.error);
+        }
+    },
+    ajaxError: function (response) {
+        this.isAjaxing = false;
+
+        if (!response.detail.response.error) {
+            console.log(response.detail.response);
+            this.countries = response.detail.response;
+        } else {
+            console.log(response.detail.response.error);
+        }
+    }
+});
---
+++ UCosmic.Web.Mvc/components/pages/Locations/index.ts
@@ -0,0 +1,32 @@
+﻿/// <reference path="../../typediff/mytypes.d.ts" />
+
+Polymer('is-page-locations-index', {
+    isAjaxing: false,
+    ready: function () {
+
+    },
+    countriesResponse: function (response) {
+        this.isAjaxing = false;
+
+        if (!response.detail.response.error) {
+            console.log(response.detail.response)
+            this.countries = response.detail.response
+        } else {
+
+            console.log(response.detail.response.error)
+        }
+
+    },
+    ajaxError: function (response) {
+        this.isAjaxing = false;
+
+        if (!response.detail.response.error) {
+            console.log(response.detail.response)
+            this.countries = response.detail.response
+        } else {
+
+            console.log(response.detail.response.error)
+        }
+
+    },
+}); 
