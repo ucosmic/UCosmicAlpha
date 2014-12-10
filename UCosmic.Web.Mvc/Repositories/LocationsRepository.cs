ab9ce1244bfb98f25129a8393d719e96d169bff5
timbow574521 <timtwillis@gmail.com>
2014-12-10 11:18:59 -0500

    Update locationsRepository to correctly bring down activities by establishment and place.

--- UCosmic.Web.Mvc/Repositories/LocationsRepository.cs
+++ UCosmic.Web.Mvc/Repositories/LocationsRepository.cs
@@ -20,27 +20,66 @@ namespace UCosmic.Repositories
             SqlConnectionFactory connectionFactory = new SqlConnectionFactory();
             // decrypt the password. Use this once all passwords have been encrypted.
             // string pw = FormsAuthentication.HashPasswordForStoringInConfigFile(password.Trim(), "sha1");
-            string sql = "select av.revisionid as id, pp.officialname, eat.type  FROM [UCosmicTest].[ActivitiesV2].[ActivityLocation] al" + 
-                  " inner join [UCosmicTest].[ActivitiesV2].[ActivityValues] av on al.activityValuesId=av.revisionid" +
-                  " inner join [UCosmicTest].Places.place pp on al.placeId=pp.revisionid" +
-                  " inner join [UCosmicTest].[ActivitiesV2].[Activity] aa on av.activityId=aa.revisionid" +
-                  " inner join [UCosmicTest].[People].Person people on aa.personId=people.revisionid" +
-                  " inner join [UCosmicTest].[identity].[user] iu on iu.personId=people.revisionid" +
-                  " inner join [UCosmicTest].[ActivitiesV2].[ActivityType] at on at.activityValuesId=av.revisionid" +
-                  " inner join [UCosmicTest].employees.employeeactivitytype eat on eat.establishmentId=iu.tenantId and eat.id = at.typeId" +
-                  " where iu.tenantId=" + EstablishmentId;
+            string sql = "select aa.revisionid as id, pp.officialname, eat.type  FROM [ActivitiesV2].[ActivityLocation] al" +
+                  " inner join [ActivitiesV2].[ActivityValues] av on al.activityValuesId=av.revisionid" +
+                  " inner join Places.place pp on al.placeId=pp.revisionid" +
+                  " inner join [ActivitiesV2].[Activity] aa on av.activityId=aa.revisionid" +
+                  " inner join [People].Person people on aa.personId=people.revisionid" +
+                  " left outer join people.affiliation pa on pa.personId=people.revisionid" +
+                  " inner join [identity].[user] iu on iu.personId=people.revisionid" +
+                  " left outer join [ActivitiesV2].[ActivityType] at on at.activityValuesId=av.revisionid" +
+                  " left outer join employees.employeeactivitytype eat on eat.establishmentId=iu.tenantId and eat.id = at.typeId" +
+                  " left outer join establishments.establishmentNode een on pa.establishmentid=een.offspringId" +
+                  " where (pa.establishmentid=" + EstablishmentId + " or een.AncestorId=" + EstablishmentId + ") and aa.mode='public' and av.mode='public' and aa.EditSourceId is null";
             if (PlaceId > 0)
             {
                 sql += " and pp.revisionid=" + PlaceId;
             }
-            //const string sql = "select eat.[type],  count(eat.[type]) as typeCount  FROM [UCosmicTest].[ActivitiesV2].[ActivityLocation] al" +
-            //      " inner join [UCosmicTest].[ActivitiesV2].[ActivityValues] av on al.activityValuesId=av.revisionid" +
-            //      " inner join [UCosmicTest].Places.place pp on al.placeId=pp.revisionid" +
-            //      " inner join [UCosmicTest].[ActivitiesV2].[Activity] aa on av.activityId=aa.revisionid" +
-            //      " inner join [UCosmicTest].[People].Person people on aa.personId=people.revisionid" +
-            //      " inner join [UCosmicTest].[identity].[user] iu on iu.personId=people.revisionid" +
-            //      " inner join [UCosmicTest].[ActivitiesV2].[ActivityType] at on at.activityValuesId=av.revisionid" +
-            //      " inner join [UCosmicTest].employees.employeeactivitytype eat on eat.establishmentId=iu.tenantId" +
+            //const string sql = "select eat.[type],  count(eat.[type]) as typeCount  FROM [ActivitiesV2].[ActivityLocation] al" +
+            //      " inner join [ActivitiesV2].[ActivityValues] av on al.activityValuesId=av.revisionid" +
+            //      " inner join Places.place pp on al.placeId=pp.revisionid" +
+            //      " inner join [ActivitiesV2].[Activity] aa on av.activityId=aa.revisionid" +
+            //      " inner join [People].Person people on aa.personId=people.revisionid" +
+            //      " inner join [identity].[user] iu on iu.personId=people.revisionid" +
+            //      " inner join [ActivitiesV2].[ActivityType] at on at.activityValuesId=av.revisionid" +
+            //      " inner join employees.employeeactivitytype eat on eat.establishmentId=iu.tenantId" +
+            //      " where iu.tenantId=3306 group by eat.[type]";
+            //connectionFactory.
+            IList<ActivityLocationsApiQueryResultModel> activityLocations = connectionFactory.SelectList<ActivityLocationsApiQueryResultModel>(DB.UCosmic, sql);
+            //IList<ActivityLocationsApiModel> activityLocations = connectionFactory.SelectList<ActivityLocationsApiModel>(DB.UCosmic, sql, new { un = username });
+
+            return activityLocations;
+        }
+        public IList<ActivityLocationsApiQueryResultModel> LocationsByEstablishment_Place(int? EstablishmentId, int? PlaceId)
+        {
+
+
+            SqlConnectionFactory connectionFactory = new SqlConnectionFactory();
+            // decrypt the password. Use this once all passwords have been encrypted.
+            // string pw = FormsAuthentication.HashPasswordForStoringInConfigFile(password.Trim(), "sha1");
+            string sql = "select aa.revisionid as id, pp.officialname, eat.type  FROM [ActivitiesV2].[ActivityLocation] al" +
+                  " inner join [ActivitiesV2].[ActivityValues] av on al.activityValuesId=av.revisionid" +
+                  " inner join Places.place pp on al.placeId=pp.revisionid" +
+                  " inner join [ActivitiesV2].[Activity] aa on av.activityId=aa.revisionid" +
+                  " inner join [People].Person people on aa.personId=people.revisionid" +
+                  " left outer join people.affiliation pa on pa.personId=people.revisionid" +
+                  " inner join [identity].[user] iu on iu.personId=people.revisionid" +
+                  " left outer join [ActivitiesV2].[ActivityType] at on at.activityValuesId=av.revisionid" +
+                  " left outer join employees.employeeactivitytype eat on eat.establishmentId=iu.tenantId and eat.id = at.typeId" +
+                  " left outer join establishments.establishmentNode een on pa.establishmentid=een.offspringId" +
+                  " where (pa.establishmentid=" + EstablishmentId + " or een.AncestorId=" + EstablishmentId + ") and aa.mode='public' and av.mode='public' and aa.EditSourceId is null";
+            if (PlaceId > 0)
+            {
+                sql += " and pp.revisionid=" + PlaceId;
+            }
+            //const string sql = "select eat.[type],  count(eat.[type]) as typeCount  FROM [ActivitiesV2].[ActivityLocation] al" +
+            //      " inner join [ActivitiesV2].[ActivityValues] av on al.activityValuesId=av.revisionid" +
+            //      " inner join Places.place pp on al.placeId=pp.revisionid" +
+            //      " inner join [ActivitiesV2].[Activity] aa on av.activityId=aa.revisionid" +
+            //      " inner join [People].Person people on aa.personId=people.revisionid" +
+            //      " inner join [identity].[user] iu on iu.personId=people.revisionid" +
+            //      " inner join [ActivitiesV2].[ActivityType] at on at.activityValuesId=av.revisionid" +
+            //      " inner join employees.employeeactivitytype eat on eat.establishmentId=iu.tenantId" +
             //      " where iu.tenantId=3306 group by eat.[type]";
             //connectionFactory.
             IList<ActivityLocationsApiQueryResultModel> activityLocations = connectionFactory.SelectList<ActivityLocationsApiQueryResultModel>(DB.UCosmic, sql);
