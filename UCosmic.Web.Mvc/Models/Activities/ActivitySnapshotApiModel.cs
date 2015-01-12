9ac18c2c00b146a3439c1c4f4038e56849393212
timbow574521 <timtwillis@gmail.com>
2015-01-12 08:31:31 -0500

    Create Activities snapshotapiModel.

---
+++ UCosmic.Web.Mvc/Models/Activities/ActivitySnapshotApiModel.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using UCosmic.Domain.Languages;
+
+namespace UCosmic.Web.Mvc.Models
+{
+    public class ActivitySnapshotApiQueryResultModel
+    {
+        public int id { get; set; }
+        public int placeId { get; set; }
+        public int personId { get; set; }
+    }
+    //public class ActivitySnapshotApiModel
+    //{
+    //    public int LocationCount { get; set; }
+    //    public int TypeCount { get; set; }
+    //    public int personId { get; set; }
+
+    //    //public ActivityLocationsApiModel(int locationCount, int typeCount, string type)//cannot use a constructor because it creates an error with the JSON return...
+    //    //{
+    //    //    LocationCount = locationCount;
+    //    //    TypeCount = typeCount;
+    //    //    Type = type;
+    //    //}
+    //}
+}
