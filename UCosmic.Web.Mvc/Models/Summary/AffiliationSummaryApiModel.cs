d453a7b12e56547b4ecdbc8ae327f6cea736f9ec
timbow574521 <timtwillis@gmail.com>
2015-01-12 08:51:57 -0500

    Create summary affiliations api model.

---
+++ UCosmic.Web.Mvc/Models/Summary/AffiliationSummaryApiModel.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using UCosmic.Domain.Languages;
+
+namespace UCosmic.Web.Mvc.Models
+{
+    public class AffiliationSummaryApiQueryResultModel
+    {
+        public int AffiliationId { get; set; }
+        public string personId { get; set; }
+        public string institution { get; set; }
+        //public string placeId { get; set; }
+    }
+    public class AffiliationSummaryApiModel
+    {
+        //public int LocationCount { get; set; }
+        public int PersonCount { get; set; }
+        public int EstablishmentCount { get; set; }
+        public int AffiliationCount { get; set; }
+
+    }
+}
