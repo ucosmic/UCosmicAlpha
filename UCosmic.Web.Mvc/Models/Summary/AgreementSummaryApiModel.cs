c57fb348f50f4bb2aa5d132154c7cfae368441b4
timbow574521 <timtwillis@gmail.com>
2015-01-12 08:53:20 -0500

    Create summary agreement api model.

---
+++ UCosmic.Web.Mvc/Models/Summary/AgreementSummaryApiModel.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using UCosmic.Domain.Languages;
+
+namespace UCosmic.Web.Mvc.Models
+{
+    public class AgreementSummaryApiQueryResultModel
+    {
+        public int id { get; set; }
+        public string officialName { get; set; }
+        public string type { get; set; }
+    }
+    public class AgreementSummaryApiModel
+    {
+        public int LocationCount { get; set; }
+        public int TypeCount { get; set; }
+        public string Type { get; set; }
+        public int TypeId { get; set; }
+
+        //public ActivityLocationsApiModel(int locationCount, int typeCount, string type)//cannot use a constructor because it creates an error with the JSON return...
+        //{
+        //    LocationCount = locationCount;
+        //    TypeCount = typeCount;
+        //    Type = type;
+        //}
+    }
+}
