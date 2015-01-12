e3cb25da6b50b3f3516423dd0cfe07e313f2498a
timbow574521 <timtwillis@gmail.com>
2015-01-12 08:54:40 -0500

    Create summary expertise api model.

---
+++ UCosmic.Web.Mvc/Models/Summary/ExpertiseSummaryApiModel.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using UCosmic.Domain.Languages;
+
+namespace UCosmic.Web.Mvc.Models
+{
+    public class ExpertiseSummaryApiQueryResultModel
+    {
+        public int ExpertiseId { get; set; }
+        public string personId { get; set; }
+        public string description { get; set; }
+        //public string placeId { get; set; }
+    }
+    public class ExpertiseSummaryApiModel
+    {
+        public int LocationCount { get; set; }
+        public int PersonCount { get; set; }
+        //public int EstablishmentCount { get; set; }
+        public int ExpertiseCount { get; set; }
+
+    }
+}
