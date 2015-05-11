24647e1ef335a7414b1da5161f2b3bdd43865390
timbow574521 <timtwillis@gmail.com>
2015-05-10 20:22:21 -0400

    Make fadein, fadout, shrink, grow into a web component.

---
+++ UCosmic.Web.Mvc/components/components/animations/animations.js
@@ -0,0 +1,59 @@
+Polymer('is-animations', {
+    fadeOut: function (element, duration, callback) {
+        if (callback === void 0) { callback = null; }
+        element.style.display = "block";
+        var animation = new Animation(element, [
+            { opacity: "1.0" },
+            { opacity: "0" },
+        ], { duration: duration, fill: 'forwards' });
+        var player = document.timeline.play(animation);
+        player.addEventListener('finish', function () {
+            element.style.display = "none";
+            if (callback) {
+                callback();
+            }
+        });
+    },
+    fadeIn: function (element, duration, callback) {
+        if (callback === void 0) { callback = null; }
+        element.style.display = "block";
+        var animation = new Animation(element, [
+            { opacity: "0" },
+            { opacity: "1.0" },
+        ], { duration: duration, fill: 'forwards' });
+        var player = document.timeline.play(animation);
+        player.addEventListener('finish', function () {
+            if (callback) {
+                callback();
+            }
+        });
+    },
+    grow: function (element, duration, callback) {
+        if (callback === void 0) { callback = null; }
+        element.style.display = "block";
+        var animation = new Animation(element, [
+            { transform: "scale(0)" },
+            { transform: "scale(1)" },
+        ], { duration: duration, fill: 'forwards' });
+        var player = document.timeline.play(animation);
+        player.addEventListener('finish', function () {
+            if (callback) {
+                callback();
+            }
+        });
+    },
+    shrink: function (element, duration, callback) {
+        if (callback === void 0) { callback = null; }
+        element.style.display = "block";
+        var animation = new Animation(element, [
+            { transform: "scale(1)" },
+            { transform: "scale(0)" },
+        ], { duration: duration, fill: 'forwards' });
+        var player = document.timeline.play(animation);
+        player.addEventListener('finish', function () {
+            if (callback) {
+                callback();
+            }
+        });
+    },
+});
