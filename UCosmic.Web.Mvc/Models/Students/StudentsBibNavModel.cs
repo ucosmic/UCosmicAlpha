009eadf98860bb11579984a9684e80c927a74f64
timbow574521 <timtwillis@gmail.com>
2015-06-11 15:37:55 -0400

    Added students bibnavmodel model.

---
+++ UCosmic.Web.Mvc/Models/Students/StudentsBibNavModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace UCosmic.Web.Mvc.Models
+{
+    public class StudentsBibNavModel
+    {
+        public bool Snapshot { get; set; }
+        public bool Table { get; set; }
+        public bool Maps { get; set; }
+        public bool New { get; set; }
+        public KeyValuePair<string, string> Other { get; set; }
+        public string Custom { get; set; }
+    }
+}
