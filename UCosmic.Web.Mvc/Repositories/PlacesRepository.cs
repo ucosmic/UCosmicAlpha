500d9a94082e3e19b8b4780d14d9694edde97219
timbow574521 <timtwillis@gmail.com>
2015-06-11 15:50:30 -0400

    Add placeslist repository to get a list of places based on tenantancy.

---
+++ UCosmic.Web.Mvc/Repositories/PlacesRepository.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+//using UCosmic.Exceptions;
+using UCosmic.Interfaces;
+using UCosmic.Factories;
+using System.Web.Security;
+using System;
+using System.Data.Entity;
+using UCosmic.Domain.Places;
+using UCosmic.Web.Mvc.Models;
+using System.Data.SqlClient;
+using Dapper;
+using UCosmic.Domain.Establishments;
+
+
+namespace UCosmic.Repositories
+{
+
+    public class PlacesRepository
+    {
+        private SqlConnectionFactory connectionFactory = new SqlConnectionFactory();
+
+        public IList<string> getContinentList()
+        {
+            const string sql = @"SELECT ContinentName FROM Places.GeoNamesCountry
+                                Group By ContinentName
+                                Order By ContinentName ASC";
+
+            return connectionFactory.SelectList<string>(DB.UCosmic, sql);
+        }
+
+        public IList<string> getCountryList()
+        {
+            const string sql = @"SELECT Name FROM Places.GeoNamesCountry
+                                Order By Name ASC";
+
+            return connectionFactory.SelectList<string>(DB.UCosmic, sql);
+        }
+
+        public IList<GeoNamesCountry> getCountryRecordList()
+        {
+            const string sql = @"SELECT * FROM Places.GeoNamesCountry
+                                Order By Name ASC";
+            return connectionFactory.SelectList<GeoNamesCountry>(DB.UCosmic, sql);
+        }
+
+        public IList<Establishment> getEstablishments()
+        {
+            const string sql = @"SELECT * FROM Establishments.Establishment";
+            return connectionFactory.SelectList<Establishment>(DB.UCosmic, sql);
+        }
+    }  
+}
