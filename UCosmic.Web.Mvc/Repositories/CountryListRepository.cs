ac71737de8b77936d147652c39fec753fb3a66f8
timbow574521 <timtwillis@gmail.com>
2015-06-11 15:49:59 -0400

    Add countrylist repository to get a list of countries based on tenantancy.

---
+++ UCosmic.Web.Mvc/Repositories/CountryListRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+//using UCosmic.Exceptions;
+using UCosmic.Interfaces;
+using UCosmic.Factories;
+using System.Web.Security;
+using System;
+using UCosmic.Domain.Places;
+using UCosmic.Web.Mvc.Models;
+
+
+namespace UCosmic.Repositories
+{
+    public class CountryListAllApiReturn
+    {
+        public int country;
+        public string official_name;
+    }
+    public class CountryListAllRepository// : ISummaryRepository
+	{
+
+        public IList<CountryListAllApiReturn> Country_List_All()
+        {
+
+
+            SqlConnectionFactory connectionFactory = new SqlConnectionFactory();
+            string sql = "SELECT [RevisionId] as country, [OfficialName] as official_name " +
+                "FROM [UCosmicTest].[Places].[Place] " +
+                "where iscountry=1";
+            IList<CountryListAllApiReturn> activityLocations = connectionFactory.SelectList<CountryListAllApiReturn>(DB.UCosmic, sql);
+            
+            return activityLocations;
+        }
+        
+	}
+
+}
