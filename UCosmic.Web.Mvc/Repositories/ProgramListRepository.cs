43c4c1f49f3375a34c03d07886db6a848d2ba06e
timbow574521 <timtwillis@gmail.com>
2015-06-11 15:51:13 -0400

    Add programslist repository to get a list of programs.

---
+++ UCosmic.Web.Mvc/Repositories/ProgramListRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+//using UCosmic.Exceptions;
+using UCosmic.Interfaces;
+using UCosmic.Factories;
+using System.Web.Security;
+using System;
+using UCosmic.Domain.Places;
+using UCosmic.Web.Mvc.Models;
+
+
+namespace UCosmic.Repositories
+{
+    public class ProgramListAllApiReturn
+    {
+        public string program;
+        public string name;
+        public bool is_standard;
+        public string establishment_id;
+    }
+    public class ProgramListAllRepository// : ISummaryRepository
+	{
+
+        public IList<ProgramListAllApiReturn> Program_List_All()
+        {
+
+
+            SqlConnectionFactory connectionFactory = new SqlConnectionFactory();
+            string sql = "SELECT code as program, name, isStandard as is_standard, establishmentid as establishment_id " +
+                "FROM [UCosmicTest].[Students].[studentprogramdata] ";
+            IList<ProgramListAllApiReturn> activityLocations = connectionFactory.SelectList<ProgramListAllApiReturn>(DB.UCosmic, sql);
+            
+            return activityLocations;
+        }
+        
+	}
+
+}
