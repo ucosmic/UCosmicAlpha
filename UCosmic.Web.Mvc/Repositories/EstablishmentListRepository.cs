f3cce8f9de9d8b11e4dba5d97a15615bd9aacdbc
timbow574521 <timtwillis@gmail.com>
2015-06-11 15:48:13 -0400

    Add establishmentlist repository to get a list of establishments based on tenantancy.

--- UCosmic.Web.Mvc/Repositories/EstablishmentListRepository.cs
+++ UCosmic.Web.Mvc/Repositories/EstablishmentListRepository.cs
@@ -15,6 +15,11 @@ namespace UCosmic.Repositories
         public int forestablishmentId;
         public string text;
     }
+    public class EstablishmentListAllApiReturn
+    {
+        public int establishment;
+        public string official_name;
+    }
     public class EstablishmentListRepository// : ISummaryRepository
 	{
 
@@ -33,4 +38,24 @@ namespace UCosmic.Repositories
         }
         
 	}
+    public class EstablishmentListAllRepository// : ISummaryRepository
+	{
+
+        public IList<EstablishmentListAllApiReturn> EstablishmentList_All_By_establishment()
+        {
+
+
+            SqlConnectionFactory connectionFactory = new SqlConnectionFactory();
+            string sql = "SELECT ee.[RevisionId] as establishment," +
+	            "Case when en.translationtolanguageid=1 then [text] else officialName end as official_name " +
+                "FROM [UCosmicPreview].[Establishments].[Establishment] ee " +
+                "left join establishments.establishmentname en on en.forestablishmentid = ee.revisionid and en.translationtolanguageid=1 " +
+	            "where ee.iscurrent=1 and ee.isdeleted=0";
+            IList<EstablishmentListAllApiReturn> activityLocations = connectionFactory.SelectList<EstablishmentListAllApiReturn>(DB.UCosmic, sql);
+            
+            return activityLocations;
+        }
+        
+	}
+
 }
