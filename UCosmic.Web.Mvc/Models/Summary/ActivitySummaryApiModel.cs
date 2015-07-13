7fb149da12f9aa74b278857667858306203c6eb6
timbow574521 <timtwillis@gmail.com>
2015-07-13 10:49:22 -0400

    Update activity summary api model to include totals.

--- UCosmic.Web.Mvc/Models/Summary/ActivitySummaryApiModel.cs
+++ UCosmic.Web.Mvc/Models/Summary/ActivitySummaryApiModel.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using System.Collections.Generic;
 using UCosmic.Domain.Languages;
 
 namespace UCosmic.Web.Mvc.Models
@@ -10,6 +11,19 @@ namespace UCosmic.Web.Mvc.Models
         public string type { get; set; }
     }
     public class ActivitySummaryApiModel
+    {
+        public int TotalLocations { get; set; }
+        public int TotalActivities { get; set; }
+        public List<ActivitySummaryTypesApiModel> ActivitySummaryTypes { get; set; }
+
+        //public ActivityLocationsApiModel(int locationCount, int typeCount, string type)//cannot use a constructor because it creates an error with the JSON return...
+        //{
+        //    LocationCount = locationCount;
+        //    TypeCount = typeCount;
+        //    Type = type;
+        //}
+    }
+    public class ActivitySummaryTypesApiModel
     {
         public int LocationCount { get; set; }
         public int TypeCount { get; set; }
