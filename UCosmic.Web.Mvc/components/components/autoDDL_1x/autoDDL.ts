db8c5a7fd09a9b2504bb881302a2909b9daf627c
timbow574521 <timtwillis@gmail.com>
2015-08-10 11:55:58 -0400

    Clean up code in autoDDL.ts

--- UCosmic.Web.Mvc/components/components/autoDDL_1x/autoDDL.ts
+++ UCosmic.Web.Mvc/components/components/autoDDL_1x/autoDDL.ts
@@ -2,26 +2,7 @@
 // attributes="url,selected,selectedid,list,backgroundcolor,highlightcolor,place_holder_text"
 Polymer({
     is: 'is-auto-ddl',
-    //behaviors: [Polymer.NeonAnimationRunnerBehavior],
     properties: {
-        //animationConfig: {
-        //    value: function () {
-        //        return {
-        //            'open': {
-        //                // provided by neon-animation/animations/scale-up-animation.html
-        //                name: 'fade-in-animation',
-        //                node: this.$.filter
-        //                //, transformOrigin: '100% 0%'// + (this.$.filter.clientWidth - 20)
-        //            },
-        //            'close': {
-        //                // provided by neon-animation-animations/fade-out-animation.html
-        //                name: 'fade-out-animation',
-        //                node: this.$.filter
-        //                //, transformOrigin: '100% 0%'
-        //            }
-        //        }
-        //    }
-        //}
         list: {
             type: Array
             , notify: true
@@ -71,55 +52,6 @@ Polymer({
     position: 0,
     isChrome: false,
     is_scrolling: false,
-    //fadeOutBackground: function (element, duration, callback = null) {
-    //    //element.style.display = "block";
-    //    //if (this.isChrome) {
-    //    var animation = new Animation(element, [
-    //        //{ opacity: "1.0", transform: "scale(1)" },
-    //        //{ opacity: "0", transform: "scale(0)" },
-    //        { backgroundColor: this.highlightcolor },
-    //        { backgroundColor: "transparent" },
-    //    ], { duration: duration, fill: 'forwards' });
-    //    var player = document.timeline.play(animation);
-    //    player.addEventListener('finish', () => {
-    //        element.style.backgroundColor = "transparent";
-    //        var mythis = this;
-    //        if (callback) {
-    //            callback();
-    //        }
-    //    });
-    //    //} else {
-    //    //    element.style.backgroundColor = "transparent";
-    //    //}
-    //},
-    //fadeInBackground: function (element, duration, position, callback = null) {
-    //    //element.style.display = "block";
-    //    //if (this.isChrome) {
-    //    var animation = new Animation(element, [
-    //        { backgroundColor: "transparent" },
-    //        { backgroundColor: this.highlightcolor },
-    //        //{ opacity: "0", transform: "scale(0)" },
-    //        //{ opacity: "1.0", transform: "scale(1)" },
-    //    ], { duration: duration, fill: 'forwards' });
-    //    //this.fadePlayer.play(animation);
-    //    var player = document.timeline.play(animation);
-    //    player.addEventListener('finish', () => {
-    //        if (position == this.position) {
-    //            element.style.backgroundColor = this.highlightcolor;
-    //            var mythis = this;
-    //            if (callback) {
-    //                callback(position);
-    //            }
-    //        } else {
-    //            //element.style.backgroundColor = "transparent";
-    //            this.fadeOutBackground(element, 0);
-    //        }
-    //    });
-    //    //} else {
-    //    //    element.style.backgroundColor = "transparent";
-    //    //}
-
-    //},
     created: function () {
         var isChromium = window.chrome,
             vendorName = window.navigator.vendor;
@@ -129,9 +61,6 @@ Polymer({
     },
     attached: function () {
         this.listChangeObserver();
-        //this.$.itemsInnerContainer.style.backgroundColor = this.backgroundcolor;
-        //this.$.itemsContainer.querySelector("#itemsInnerContainer").style.backgroundColor = this.backgroundcolor;
-        //CoreStyle.list.autoDDL.first = this.backgroundcolor;
     },
     leaveSearch: function (event, detail, sender) {
         if (!this.is_scrolling) {
@@ -251,36 +180,16 @@ Polymer({
         });
     },
 
-    //    this.fadeOut(this.$.changePage, 500);
-    //this.fadeOut(this.$.changePage2, 500);
-    //this.fadeIn(this.$.core_card_content, 200,() => {
-
-    //    this.$.core_card_content.style.display = "block";
-    //});
     hoverChange: function (newValue, speed) {
         try {
-            //if (newValue) {
-            //    var element = this.$.itemsContainer.querySelector("#itemsInnerContainer").children[this.position];
-            //    this.fadeOutBackground(element, speed);//,() => {
-            //    //this.$.itemsContainer.querySelector("#itemsInnerContainer").children[this.position].style.backgroundColor = "";
-            //} else {
-            //var element = this.$.itemsContainer.querySelector("#itemsInnerContainer").children[this.position];
-
             var x = 0, elements = this.$.itemsContainer.querySelector("#itemsInnerContainer").children;
             if (elements) {
-                //elements = elements.querySelectorAll(".list-item");
                 for (x; x < elements.length; x++) {
                     if (elements[x].style.backgroundColor == this.highlightcolor) {
                         elements[x].style.backgroundColor = "transparent";
-                        //this.fadeOutBackground(elements[x], 0);
                     };
                 }
                 elements[this.position].style.backgroundColor = "yellow"; 
-                //this.fadeInBackground(elements[this.position], speed, this.position);//,() => {
-                //    element.style.display = "block";
-                //});
-                //this.$.itemsContainer.querySelector("#itemsInnerContainer").children[this.position].style.backgroundColor = this.highlightcolor;
-                //}
             } else {
                 setTimeout(() => {
                     if (this.list && this.list.length > 0) {
@@ -297,10 +206,6 @@ Polymer({
         }
     },
     selectHover: function (event, detail, sender) {
-        //var x = 0, parent = sender.parentElement.children;
-        //for (x; x < parent.length; x++) {
-        //    parent[x].style.backgroundColor = "";
-        //}
         var item = this.querySelector("#items").itemForElement(event.target);
         this.position = _.findIndex(this.list, item);// + 1;
         this.hoverChange(false, 100);
@@ -317,11 +222,9 @@ Polymer({
                 }
             }, 100);
             this.position = 0;
-            //this.hoverChange(false,200);
         }
     },
     selectHoverLeave: function (event, detail, sender) {
-        //this.position = event.target.templateInstance.model.itemI + 1;
         var item = this.querySelector("#items").itemForElement(event.target);
         this.position = _.findIndex(this.list, item);// + 1;
         this.hoverChange(true, 100);
