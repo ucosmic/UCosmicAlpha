dbc43f5eb0614482ece7d4f8fedc21ffef59f740
timbow574521 <timtwillis@gmail.com>
2015-08-10 11:48:30 -0400

    Update activitysummaryapimodel for type and person count.

--- UCosmic.Web.Mvc/Models/Summary/ActivitySummaryApiModel.cs
+++ UCosmic.Web.Mvc/Models/Summary/ActivitySummaryApiModel.cs
@@ -9,11 +9,14 @@ namespace UCosmic.Web.Mvc.Models
         public int id { get; set; }
         public string officialName { get; set; }
         public string type { get; set; }
+        public int type_id { get; set; }
+        public int person_id { get; set; }
     }
     public class ActivitySummaryApiModel
     {
         public int TotalLocations { get; set; }
         public int TotalActivities { get; set; }
+        public int TotalPeople { get; set; }
         public List<ActivitySummaryTypesApiModel> ActivitySummaryTypes { get; set; }
 
         //public ActivityLocationsApiModel(int locationCount, int typeCount, string type)//cannot use a constructor because it creates an error with the JSON return...
@@ -27,6 +30,7 @@ namespace UCosmic.Web.Mvc.Models
     {
         public int LocationCount { get; set; }
         public int TypeCount { get; set; }
+        public int PersonCount { get; set; }
         public string Type { get; set; }
         public int TypeId { get; set; }
 
