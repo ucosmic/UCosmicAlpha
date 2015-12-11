077035538bcce0fddc719a887d45774826b200ca
timbow574521 <timtwillis@gmail.com>
2015-12-11 16:07:42 -0500

    Create a riot closest element utility to find the closest element to another element.

---
+++ UCosmic.Web.Mvc/components_riot/components/utilities/closest.js
@@ -0,0 +1,54 @@
+var find_closest = function (el, selector) {
+    var matchesFn;
+    // find vendor prefix
+    ['matches', 'webkitMatchesSelector', 'mozMatchesSelector', 'msMatchesSelector', 'oMatchesSelector'].some(function (fn) {
+        if (typeof document.body[fn] == 'function') {
+            matchesFn = fn;
+            return true;
+        }
+        return false;
+    });
+    // traverse parents
+    var parent, child;
+    while (el !== null) {
+        parent = el.parentElement;
+        child = parent.querySelector(selector);
+        if (el !== null && el[matchesFn](selector)) {
+            return el;
+        }
+        else if (parent !== null && parent[matchesFn](selector)) {
+            return parent;
+        }
+        else if (child) {
+            return child;
+        }
+        el = parent;
+    }
+    return null;
+};
+var find_closest_parent = function (el, selector) {
+    var matchesFn;
+    // find vendor prefix
+    ['matches', 'webkitMatchesSelector', 'mozMatchesSelector', 'msMatchesSelector', 'oMatchesSelector'].some(function (fn) {
+        if (typeof document.body[fn] == 'function') {
+            matchesFn = fn;
+            return true;
+        }
+        return false;
+    });
+    // traverse parents
+    var parent, child;
+    while (el !== null) {
+        parent = el.parentElement;
+        //child = parent.querySelector(selector);
+        if (parent !== null && parent[matchesFn](selector)) {
+            return parent;
+        }
+        //else if(child){
+        //    return child;
+        //}
+        el = parent;
+    }
+    return null;
+};
+//# sourceMappingURL=closest.js.map
