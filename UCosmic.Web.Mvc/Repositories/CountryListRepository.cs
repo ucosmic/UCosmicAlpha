9dc113b15b47661333efe5623abbc072176ef9c9
Tim.willis <timtwillis@gmail.com>
2016-03-10 09:20:44 -0500

    Fix country list repository to return correct country count for bug in advanced search map.

--- UCosmic.Web.Mvc/Repositories/CountryListRepository.cs
+++ UCosmic.Web.Mvc/Repositories/CountryListRepository.cs
@@ -14,6 +14,9 @@ namespace UCosmic.Repositories
     {
         public int country;
         public string official_name;
+        public string name;
+        public string place_type;
+        public int id;
     }
     public class CountryListAllRepository// : ISummaryRepository
 	{
@@ -22,10 +25,22 @@ namespace UCosmic.Repositories
         {
 
 
+            //SqlConnectionFactory connectionFactory = new SqlConnectionFactory();
+            //string sql = "SELECT [RevisionId] as country, [OfficialName] as official_name " +
+            //    "FROM [Places].[Place] " +
+            //    "where iscountry=1";
+
             SqlConnectionFactory connectionFactory = new SqlConnectionFactory();
-            string sql = "SELECT [RevisionId] as country, [OfficialName] as official_name " +
-                "FROM [Places].[Place] " +
-                "where iscountry=1";
+            string sql = "SELECT p.[RevisionId] as country, p.[OfficialName] as official_name, " +
+				"p2.officialName as name, p2.revisionId as id,  " +
+                "CASE WHEN p2.isregion = 1 THEN 'region' WHEN p2.iswater = 1 THEN 'water' WHEN p2.iscontinent = 1 THEN 'continent' ELSE '' End as place_type " +
+                "FROM [Places].[Place] p " +
+                "left outer join Places.GeoPlanetPlace gpp on gpp.placeId = p.revisionid " +
+                "left outer join Places.GeoPlanetPlaceBelongTo gppbt on gppbt.placeWoeId = gpp.woeId " +
+                "left outer join Places.GeoPlanetPlace gpp2 on gpp2.woeId = gppbt.belongToWoeId " +
+                "left outer join Places.Place p2 on gpp2.placeId = p2.revisionid " +
+                "and (p2.isregion = 1 or p2.iscontinent = 1 or p2.iswater=1) " +
+                "where p.iscountry=1";
             IList<CountryListAllApiReturn> activityLocations = connectionFactory.SelectList<CountryListAllApiReturn>(DB.UCosmic, sql);
             
             return activityLocations;
