f696fa9b7bc92cc5796f953b414ea639bd897de8
Tim.willis <timtwillis@gmail.com>
2016-05-11 10:48:20 -0400

    Add service worker to give significate page load speed to all pages. Change page layouts bib and module tab to not use jquery.

---
+++ UCosmic.Web.Mvc/sw.js
@@ -0,0 +1,64 @@
+﻿importScripts('/components/bower_components/sw-toolbox/sw-toolbox.js');
+
+self.addEventListener('message', function (event) {
+    console.log('good');
+    event.ports[0].postMessage({ 'test': 'This is my response.' });
+});
+//toolbox.options.debug = true;
+
+//toolbox.precache(['/*']);
+
+//toolbox.router.get('/*', toolbox.fastest);
+
+//toolbox.router.get('/^https:\/\/www.google.com\/', toolbox.fastest);
+
+toolbox.router.get('/*', toolbox.fastest);
+toolbox.router.get('/*', toolbox.fastest, { origin: 'https://www.google.com' });
+
+
+toolbox.router.get('/*', toolbox.fastest, { origin: 'https://api.google.com' });
+
+toolbox.router.get('/(.*)', toolbox.fastest, { origin: 'https://www.google.com' });
+
+
+toolbox.router.get('/(.*)', toolbox.fastest, { origin: 'https://api.google.com' });
+
+
+toolbox.router.get('/*', toolbox.fastest, { origin: 'http://maps.googleapis.com' });
+
+toolbox.router.get('/(.*)', toolbox.fastest, { origin: 'http://maps.googleapis.com' });
+
+
+//toolbox.router.get('*/*', toolbox.fastest);
+
+
+//toolbox.router.get('/^https:\/\/', toolbox.fastest);
+//sw-precache 
+
+
+//toolbox.router.get('/*', function (test) {
+//    var x = test;
+//});
+
+//toolbox.router.get('/^https:\/\/www.google.com\/', function (test) {
+//    var x = test;
+//});
+
+//toolbox.router.get('*', function (test) {
+//    var x = test;
+//}); 
+
+//toolbox.router.get('*', function (test) {
+//    var x = test; 
+//    return x;
+//}, { origin: 'https://www.google.com' });
+
+
+//toolbox.router.get('*', function (test) {
+//    var x = test;
+//}, { origin: 'https://api.google.com' });
+
+
+//toolbox.router.get('/^https:\/\/', function (test) {
+//    var x = test;
+//});
