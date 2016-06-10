6412b15be3763c88a2efa9b784c8d1a3c1898290
Tim.willis <timtwillis@gmail.com>
2016-06-10 15:45:18 -0400

    Fix bug in activity repository to fix the date sorting for faculty activities.

--- UCosmic.Web.Mvc/Repositories/ActivityRepository.cs
+++ UCosmic.Web.Mvc/Repositories/ActivityRepository.cs
@@ -33,22 +33,6 @@ namespace UCosmic.Repositories
                 }
                 sql += ")";
             }
-            //if (input.ActivityTypeIds != null && input.ActivityTypeIds.Length > 0 && input.ActivityTypeIds[0] != 0)
-            //{
-            //    sql += " and (";
-            //    foreach (var activityTypeId in input.ActivityTypeIds.Select((x, i) => new { Value = x, Index = i }))
-            //    {
-            //        if (activityTypeId.Index > 0)
-            //        {
-            //            sql += " or at.typeId=" + activityTypeId.Value;
-            //        }
-            //        else
-            //        {
-            //            sql += " at.typeId=" + activityTypeId.Value;
-            //        }
-            //    }
-            //    sql += ")";
-            //}
             if (input.PlaceNames != null && input.PlaceNames.Length > 0 && input.PlaceNames[0] != null && input.PlaceNames[0].Length > 0)
             {
                 sql += " and (";
@@ -65,79 +49,6 @@ namespace UCosmic.Repositories
                 }
                 sql += ")";
             }
-            //if (input.IncludeUndated != null)
-            //{
-            //    if (input.IncludeUndated == false)
-            //    {
-            //        if (input.Since != null && input.Since != "" && input.Until != null && input.Until != "")
-            //        {
-            //            sql += " and ((av.startson <= '" + input.Until + "'or av.startson is null) and ((av.endson >= '" + input.Since + "' or av.ongoing=1) or av.endson is null))";
-            //            sql += " and ((av.startson is not null) and (av.endson is not null))";
-            //            //sql += " and (av.startson <= '" + input.Until + "' and av.endson >= '" + input.Since + "' or av.ongoing=1)";
-            //        }
-            //        else if (input.Since != null && input.Since != "")
-            //        {
-            //            sql += " and (av.startson >= '" + input.Since + "' or av.endson >= '" + input.Since + "' or av.ongoing=1)";
-            //        }
-            //        else if (input.Until != null && input.Until != "")
-            //        {
-            //            sql += " and (av.endson <= '" + input.Until + "' or av.startson <= '" + input.Until + "')";
-            //        }
-            //        //if ((input.Until == null || input.Until == "") && (input.Since == null || input.Since == ""))
-            //        else
-            //        {
-            //            sql += " and (av.endson <= '" + DateTime.Now + "' or av.startson <= '" + DateTime.Now + "')";
-            //        }
-            //    }
-            //    else
-            //    {
-            //        if (input.Since != null && input.Since != "" && input.Until != null && input.Until != "")
-            //        {
-            //            sql += " and ((av.startson <= '" + input.Until + "'or av.startson is null) and ((av.endson >= '" + input.Since + "' or av.ongoing=1) or av.endson is null))";
-            //        }
-            //        else if (input.Since != null && input.Since != "")
-            //        {
-            //            sql += " and (av.startson >= '" + input.Since + "' or av.startson is null or av.endson >= '" + input.Since + "' or av.ongoing=1)";
-            //        }
-            //        else if (input.Until != null && input.Until != "")
-            //        {
-            //            sql += " and (av.endson <= '" + input.Until + "' or av.endson is null or av.startson <= '" + input.Until + "')";
-            //        }
-            //        //if (input.Since != null && input.Since != "")
-            //        //{
-            //        //    sql += " and (av.startson >= '" + input.Since + "' or av.startson is null or av.endson >= '" + input.Since + "' or av.ongoing=1)";
-            //        //}
-            //        //if (input.Until != null && input.Until != "")
-            //        //{
-            //        //    sql += " and (av.endson <= '" + input.Until + "' or av.endson is null or av.startson <= '" + input.Until + "')";
-            //        //}
-            //    }
-            //}
-            //else
-            //{
-            //    if (input.Since != null && input.Since != "" && input.Until != null && input.Until != "")
-            //    {
-            //        sql += " and ((av.startson <= '" + input.Until + "'or av.startson is null) and ((av.endson >= '" + input.Since + "' or av.ongoing=1) or av.endson is null))";
-            //        sql += " and ((av.startson is not null) and (av.endson is not null))";
-            //        //sql += " and (av.startson <= '" + input.Until + "' and av.endson >= '" + input.Since + "' or av.ongoing=1)";
-            //    }
-            //    else if (input.Since != null && input.Since != "")
-            //    {
-            //        sql += " and (av.startson >= '" + input.Since + "' or av.endson >= '" + input.Since + "' or av.ongoing=1)";
-            //    }
-            //    else if (input.Until != null && input.Until != "")
-            //    {
-            //        sql += " and (av.endson <= '" + input.Until + "' or av.startson <= '" + input.Until + "')";
-            //    }
-            //    //if (input.Since != null && input.Since != "")
-            //    //{
-            //    //    sql += " and av.startson >= '" + input.Since + "' or av.endson >= '" + input.Since + "' or av.ongoing=1";
-            //    //}
-            //    //if (input.Until != null && input.Until != "")
-            //    //{
-            //    //    sql += " and (av.endson <= '" + input.Until + "' or av.startson <= '" + input.Until + "'";
-            //    //}
-            //}
             if (input.Keyword != null && input.Keyword != "")
             {
                 List<string> keywords;
@@ -274,158 +185,55 @@ namespace UCosmic.Repositories
                     {
                         if (ascDesc.Contains("desc"))
                         {
-                            sql += " order by onGoingSort desc, startsOn desc, endsOn desc, locationName asc, lastNameSort asc, firstNameSort asc, av.title asc";
+                            sql += " order by onGoingSort desc, endsOnCalc desc, startsOnCalc desc, locationName asc, lastNameSort asc, firstNameSort asc, av.title asc";
                         }
                         else
                         {
-                            sql += " order by onGoingSort asc, startsOn asc, endsOn asc, locationName asc, lastNameSort asc, firstNameSort asc, av.title asc";
+                            sql += " order by onGoingSort asc, endsOnCalc asc, startsOnCalc asc, locationName asc, lastNameSort asc, firstNameSort asc, av.title asc";
                         }
                     }
                     else if (orderBy.Contains("lastname"))
                     {
                         orderBy = "lastNameSort";
-                        sql += " order by " + orderBy + " " + ascDesc + ", firstNameSort asc, onGoingSort desc, endsOn desc, startsOn desc, locationName asc, av.title asc";
+                        sql += " order by " + orderBy + " " + ascDesc + ", firstNameSort asc, onGoingSort desc, endsOnCalc desc, startsOnCalc desc, locationName asc, av.title asc";
                     }
                     else if (orderBy.Contains("locationName"))
                     {
-                        sql += " order by " + orderBy + " " + ascDesc + ", onGoingSort desc, endsOn desc, startsOn desc, lastNameSort asc, firstNameSort asc, av.title asc";
+                        sql += " order by " + orderBy + " " + ascDesc + ", onGoingSort desc, endsOnCalc desc, startsOnCalc desc, lastNameSort asc, firstNameSort asc, av.title asc";
                     }
                     else if (orderBy.Contains("title"))
                     {
-                        sql += " order by " + orderBy + " " + ascDesc + ", onGoingSort desc, endsOn desc, startsOn desc, locationName asc, lastNameSort asc, firstNameSort asc";
+                        sql += " order by " + orderBy + " " + ascDesc + ", onGoingSort desc, endsOnCalc desc, startsOnCalc desc, locationName asc, lastNameSort asc, firstNameSort asc";
                     }
                     else
                     {
-                        sql += " order by " + orderBy + " " + ascDesc + ", onGoingSort desc, endsOn desc, startsOn desc, locationName asc, lastNameSort asc, firstNameSort asc, av.title asc";
+                        sql += " order by " + orderBy + " " + ascDesc + ", onGoingSort desc, endsOnCalc desc, startsOnCalc desc, locationName asc, lastNameSort asc, firstNameSort asc, av.title asc";
                     }
 
                 }
             }
             else
             {
-                sql += " order by onGoingSort desc, endsOn desc, startsOn desc, locationName asc, lastNameSort asc, firstNameSort asc, av.title asc";
+                sql += " order by onGoingSort desc, endsOnCalc desc, startsOnCalc desc, locationName asc, lastNameSort asc, firstNameSort asc, av.title asc";
 
             }
 
-
-
-            //if (orderBy.IndexOf("desc") > -1)
-            //{
-            //    orderBy = orderBy.Replace("-desc", "");
-            //    sql += " ORDER BY " + orderBy + " desc";
-            //}
-            //else
-            //{
-            //    orderBy = orderBy.Replace("-asc", "");
-            //    sql += " ORDER BY " + orderBy + " asc";
-            //}
-
-
-            //sql = sql.Replace("replaceMeOrderBy", "t." + orderBy);
 
 
             IList<ActivityQueryResultModel> ActivityQueryResult = connectionFactory.SelectList<ActivityQueryResultModel>(DB.UCosmic, sql);
 
             return ActivityQueryResult;
         }
-        //    public IList<ActivityMapCountsApiQueryResultModel> ActivityMapCount_Water(ActivitySearchInputModel input, int? ancestorId)
-        //    {
-        //        SqlConnectionFactory connectionFactory = new SqlConnectionFactory();
-        //        string sql = "select distinct aa.revisionid, pp1.revisionid as id, people.revisionId as personId, pp1.officialName as name, pp1.latitude, pp1.longitude, code = '' FROM [ActivitiesV2].[ActivityLocation] al" +
-        //              " inner join [ActivitiesV2].[ActivityValues] av on al.activityValuesId=av.revisionid" +
-        //              " left outer join [ActivitiesV2].[ActivityType] at on at.activityValuesId=av.revisionid" +
-        //              " left outer join [ActivitiesV2].[ActivityTag] atag on atag.activityValuesId=av.revisionid" +//may not have tags
-        //              " inner join Places.place pp1 on al.placeId=pp1.revisionid" +
-        //            //" left outer join Places.place pp2 on pp1.parentid=pp2.revisionid" + // may not need but would have to change filter
-        //              " inner join [ActivitiesV2].[Activity] aa on av.activityId=aa.revisionid" +
-        //              " inner join [People].Person people on aa.personId=people.revisionid" +
-        //              " left outer join people.affiliation pa on pa.personId=people.revisionid" +
-        //              " left outer join establishments.establishmentNode een on pa.establishmentid=een.offspringId" +
-        //              " inner join [identity].[user] iu on iu.personId=people.revisionid" +
-        //              " left outer join Places.GeoPlanetPlace gpp on gpp.placeId=pp1.revisionid " +
-        //              " left outer join Places.GeoPlanetPlaceBelongTo gppbt on gppbt.placeWoeId=gpp.woeid" +
-        //              " left outer join Places.GeoPlanetPlace gpp2 on gppbt.BelongToWoeId=gpp2.woeid" +
-        //              " left outer join Places.place pp2 on gpp2.placeId=pp2.revisionid " +
-        //              " where (pa.establishmentid=" + ancestorId + " or een.AncestorId=" + ancestorId + ") and pp1.isWater = 1 and aa.mode='public' and av.mode='public' and aa.EditSourceId is null";
-        //        if (input != null)
-        //        {
-        //            sql = AddSqlFilter(sql, input);
-        //        }
-        //        //if(Type == "country"){
-        //        //    sql += "pp1.isCountry = 1";
-        //        //}
-        //        IList<ActivityMapCountsApiQueryResultModel> ActivityMapCounts = connectionFactory.SelectList<ActivityMapCountsApiQueryResultModel>(DB.UCosmic, sql);
-
-        //        return ActivityMapCounts;
-        //    }
-
-        //    public IList<ActivityMapCountsApiQueryResultModel> ActivityMapCount_Continent(ActivitySearchInputModel input, int? ancestorId)
-        //    {
-
-
-        //        SqlConnectionFactory connectionFactory = new SqlConnectionFactory();
-        //        string sql = "select distinct aa.revisionid, pp1.revisionid as id, people.revisionId as personId, pp1.latitude, pp1.longitude, pp1.isContinent, pp1.isWater, " +
-        //              " CASE WHEN (pp1.iswater = 1 and gnt.continentcode IS null) THEN 'WATER' WHEN pp1.isearth = 1 THEN 'GLOBAL' ELSE gnt.continentcode END as code, " +
-        //              " CASE WHEN (pp1.iswater = 1 and gnt.continentcode IS null) THEN 'Bodies Of Water' WHEN pp1.isearth = 1 THEN 'Global'  " +
-        //              " WHEN gnt.continentcode = 'AF' THEN 'Africa' " +
-        //              " WHEN gnt.continentcode = 'AN' THEN 'Antarctica' " +
-        //              " WHEN gnt.continentcode = 'AS' THEN 'Asia' " +
-        //              " WHEN gnt.continentcode = 'EU' THEN 'Europe' " +
-        //              " WHEN gnt.continentcode = 'NA' THEN 'North America' " +
-        //              " WHEN gnt.continentcode = 'OC' THEN 'Oceana' " +
-        //              " WHEN gnt.continentcode = 'SA' THEN 'South America'" +
-        //              " ELSE pp1.officialName END as name" +
-        //              " FROM [ActivitiesV2].[ActivityLocation] al" +
-        //              " inner join [ActivitiesV2].[ActivityValues] av on al.activityValuesId=av.revisionid" +
-        //              " left outer join [ActivitiesV2].[ActivityType] at on at.activityValuesId=av.revisionid" +
-        //              " left outer join [ActivitiesV2].[ActivityTag] atag on atag.activityValuesId=av.revisionid" +//may not have tags
-        //              " inner join Places.place pp1 on al.placeId=pp1.revisionid" +
-        //            //" left outer join Places.place pp2 on pp1.parentid=pp2.revisionid" +//may not have parent
-        //              " inner join [ActivitiesV2].[Activity] aa on av.activityId=aa.revisionid" +
-        //              " inner join [People].Person people on aa.personId=people.revisionid" +
-        //              " left outer join people.affiliation pa on pa.personId=people.revisionid" +
-        //              " left outer join establishments.establishmentNode een on pa.establishmentid=een.offspringId" +
-        //              " inner join [identity].[user] iu on iu.personId=people.revisionid" +
-        //              " inner join Places.geonamestoponym gnt on gnt.placeId=pp1.revisionid" +
-        //              " left outer join Places.GeoPlanetPlace gpp on gpp.placeId=pp1.revisionid " +
-        //              " left outer join Places.GeoPlanetPlaceBelongTo gppbt on gppbt.placeWoeId=gpp.woeid" +
-        //              " left outer join Places.GeoPlanetPlace gpp2 on gppbt.BelongToWoeId=gpp2.woeid" +
-        //              " left outer join Places.place pp2 on gpp2.placeId=pp2.revisionid " +
-        //              " where (pa.establishmentid=" + ancestorId + " or een.AncestorId=" + ancestorId + ") and aa.mode='public' and av.mode='public' and aa.EditSourceId is null" +
-        //              " and (pp1.isregion=0 or (pp1.isregion=1 and gnt.continentcode IS NOT null))";
-        //        if (input != null)
-        //        {
-        //            sql = AddSqlFilter(sql, input);
-        //        }
-        //        //if(Type == "country"){
-        //        //    sql += "pp1.isCountry = 1";
-        //        //}
-        //        IList<ActivityMapCountsApiQueryResultModel> ActivityMapCounts = connectionFactory.SelectList<ActivityMapCountsApiQueryResultModel>(DB.UCosmic, sql);
-
-        //        return ActivityMapCounts;
-        //    }
         public IList<ActivityTypesApiQueryResultModel> ActivityTypesByEstablishment(int? EstablishmentId)
         {
 
 
             SqlConnectionFactory connectionFactory = new SqlConnectionFactory();
             // decrypt the password. Use this once all passwords have been encrypted.
-            // string pw = FormsAuthentication.HashPasswordForStoringInConfigFile(password.Trim(), "sha1");
             string sql = "SELECT  id ,[Type] ,[Rank] FROM [Employees].[EmployeeActivityType] where establishmentId=3306";
 
 
-            //const string sql = "select eat.[type],  count(eat.[type]) as typeCount  FROM [ActivitiesV2].[ActivityLocation] al" +
-            //      " inner join [ActivitiesV2].[ActivityValues] av on al.activityValuesId=av.revisionid" +
-            //      " inner join Places.place pp on al.placeId=pp.revisionid" +
-            //      " inner join [ActivitiesV2].[Activity] aa on av.activityId=aa.revisionid" +
-            //      " inner join [People].Person people on aa.personId=people.revisionid" +
-            //      " inner join [identity].[user] iu on iu.personId=people.revisionid" +
-            //      " inner join [ActivitiesV2].[ActivityType] at on at.activityValuesId=av.revisionid" +
-            //      " inner join employees.employeeactivitytype eat on eat.establishmentId=iu.tenantId" +
-            //      " where iu.tenantId=3306 group by eat.[type]";
-            //connectionFactory.
             IList<ActivityTypesApiQueryResultModel> activitySnapshot = connectionFactory.SelectList<ActivityTypesApiQueryResultModel>(DB.UCosmic, sql);
-            //IList<ActivityLocationsApiModel> activityLocations = connectionFactory.SelectList<ActivityLocationsApiModel>(DB.UCosmic, sql, new { un = username });
 
             return activitySnapshot;
         }
