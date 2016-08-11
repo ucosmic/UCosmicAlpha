857ba19ed8326b3f6e49d9d6c18120451105fddf
Tim.willis <timtwillis@gmail.com>
2016-08-11 12:24:30 -0400

    Add sub affiliation(ancestorID) to the query and model for agreements by keyword search.

--- UCosmic.Domain/Domain/Agreements/Queries/AgreementsByKeyword.cs
+++ UCosmic.Domain/Domain/Agreements/Queries/AgreementsByKeyword.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using MoreLinq;
 using System.Security.Principal;
 
 namespace UCosmic.Domain.Agreements
@@ -24,6 +25,7 @@ namespace UCosmic.Domain.Agreements
         public string CountryCode { get; set; }
         public string TypeCode { get; set; }
         public string Keyword { get; set; }
+        public int AncestorId { get; set; }
     }
 
     public class HandleAgreementsByKeywordQuery : IHandleQueries<AgreementsByKeyword, PagedQueryResult<Agreement>>
@@ -80,6 +82,31 @@ namespace UCosmic.Domain.Agreements
                     || x.Type.Contains(query.Keyword)
                 );
             }
+
+            if (query.AncestorId != 0)
+            {
+                queryable = queryable.Where(x => x.Participants.Any(y => y.EstablishmentId == query.AncestorId) || x.Participants.Any(y => y.Establishment.Ancestors.Any(z => z.AncestorId == query.AncestorId)));
+            }
+
+
+            //case "sub-affiliation":
+            //queryable = queryable.OrderBy(x => x.Participants.Where(y => y.IsOwner).MaxBy(y => y.Establishment.Ancestors != null ? y.Establishment.Ancestors.MaxBy(z => z.Separation).Separation : 0).Establishment.OfficialName);
+                //? x.Participants.Where(y => y.IsOwner && y.Establishment.Ancestors. y.Establishment.Ancestors.MaxBy(z => z.Separation)).Establishment.OfficialName
+                //: x.Participants.FirstOrDefault(y => y.IsOwner).Establishment.OfficialName);
+            //int max = items.Max(i => i.ID);
+            //var item = items.First(x => x.ID == max);
+
+            //queryable = queryable.Aggregate((i1,i2) => i1.ID > i2.ID ? i1 : i2)
+
+            //var item = queryable.First(x => x.Participants.Where(y => y.IsOwner).Max(y => y.Establishment.Ancestors != null ? y.Establishment.Ancestors.MaxBy(z => z.Separation).Separation : 0) == max);
+            //queryable = queryable.OrderBy(x => x.Participants.Where(y => y.IsOwner).MaxBy(y => y.Establishment.Ancestors != null ? y.Establishment.Ancestors.MaxBy(z => z.Separation).Separation : 0).Establishment.OfficialName);
+
+
+            //*********************** this seems to be working but need to test further. Also need to add back in the orderby below
+            // also need to do an if statement if this order by is used
+            // also add in the stuff in the view and viewmodel
+            //int max = queryable.Max(x => x.Participants.Where(y => y.IsOwner).Max(y => y.Establishment.Ancestors.Any() != null ? y.Establishment.Ancestors.Max(z => z.Separation) : 0));
+            //queryable = queryable.OrderByDescending(x => x.Participants.Where(y => y.IsOwner).FirstOrDefault(y => y.Establishment.Ancestors.Any() != null ? y.Establishment.Ancestors.Max(z => z.Separation) == max : 0 == max).Establishment.OfficialName);
 
             queryable = queryable.OrderBy(query.OrderBy);
 
