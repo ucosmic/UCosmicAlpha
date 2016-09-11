6ffaf525395c30f849403b3904a59f3486cfd039
Tim.willis <timtwillis@gmail.com>
2016-09-11 09:37:12 -0400

    Create a country_list_item model for countries and there associations.

---
+++ UCosmic.Web.Mvc/Models/Places/CountryListAssociations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UCosmic.Repositories;
+
+namespace UCosmic.Web.Mvc.Models.Places
+{
+    //public class country_item
+    //{
+    //    public int id;
+    //    public string country;
+    //    public string code;
+    //}
+
+    public class country_list_item
+    {
+        public int id;
+        public string country;
+        public string code;
+        public List<CountryListAllApiReturn> associations;
+    }
+}
